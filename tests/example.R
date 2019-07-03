7d5363cd276a4fcea37c6bb683a3497e72314496
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-04 01:18:17 +0200

    added some temporary examples of use

--- tests/example.R
+++ tests/example.R
@@ -1,12 +1,37 @@
+#temporary
+
+load("data/aminoacids_df.rda")
+load("data/nucleotides_df.rda")
+
+#constructing and validating sq
+
 ex_sq1 <- construct_sq("aACccDaaa")
 ex_sq2 <- construct_aasq("PmHe")
 ex_sq3 <- construct_aasq("QQRdtvIk")
 ex_sq4 <- construct_nucsq("ACTTGGAGAGGCT")
 
-validate_sq(ex_sq1)
-validate_sq(ex_sq2)
-validate_sq(ex_sq3)
+tidysq:::validate_sq(ex_sq1)
+tidysq:::validate_sq(ex_sq2)
+tidysq:::validate_sq(ex_sq3)
 
-validate_ambsq(ex_sq1)
-validate_aasq(ex_sq2)
-validate_aasq(ex_sq3)
+tidysq:::validate_ambsq(ex_sq1)
+tidysq:::validate_aasq(ex_sq2)
+tidysq:::validate_aasq(ex_sq3)
+
+ex_sq5 <- construct_sq("asatgamktr")
+tidysq:::validate_ambsq(ex_sq5)
+ex_sq6 <- construct_sq("asatgamktr", type = "aa")
+tidysq:::validate_aasq(ex_sq6)
+ex_sq7 <- construct_sq("asatgamktr", type = "nuc")
+tidysq:::validate_nucsq(ex_sq7)
+
+#constructing and validating sqtibble
+
+#at ex_tb1 and ex_tb3 should be warnings
+ex_tb1 <- tidysq:::construct_sqtibble(c("name2", "name3", "name4"), list(ex_sq2, ex_sq3, ex_sq4))
+ex_tb2 <- tidysq:::construct_sqtibble(c("name2", "name3"), list(ex_sq2, ex_sq3))
+ex_tb3 <- tidysq:::construct_sqtibble(c("name1", "name3", "name4"), list(ex_sq1, ex_sq3, ex_sq4))
+
+tidysq:::validate_sqtibble(ex_tb1)
+tidysq:::validate_sqtibble(ex_tb2)
+tidysq:::validate_sqtibble(ex_tb3)
