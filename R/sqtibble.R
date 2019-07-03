10df1f2c781fb756dc3aea674ed6f49ad6715041
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-04 01:17:44 +0200

    added constructor and validator for sqtibble

--- R/sqtibble.R
+++ R/sqtibble.R
@@ -1,24 +1,50 @@
-#' @import tibble::tibble
-#' @exportClass sqtibble
+#' @importFrom tibble tibble
+#' @exportClass sqtbl
 #' @export
 construct_sqtibble <- function(name, sq) {
   if (!is.character(name)) {
     stop("name should be character vector")
   }
-  # after adding validate :
-  # if (!(is.list(sq) &&
-  #       all(validate_sq(sq)))) {
-  #   stop("sq should be list of sq objects")
-  # }
-  if (any(isClass("aasq")) && any(isClass("nucsq"))) {
-    #later it could be a option of package:
-    warning("sq contains both nuc and aa sequences - not all functions can handle that")
+  if (!(is.list(sq))) {
+    stop("sq should be list of 'sq' objects")
   }
-  if (any(isClass("ambsq"))) {
+  sapply(sq, validate_sq)
+  if (any(sapply(sq, function(obj) "aasq" %in% class(obj))) && 
+      any(sapply(sq, function(obj) "nucsq" %in% class(obj)))) {
     #later it could be a option of package:
-    warning("sq contains at least one amb sequence - not all functions can handle that")
+    warning("column sq contains both nuc and aa sequences - not all functions can handle that", call. = FALSE)
+  }
+  if (any(sapply(sq, function(obj) "ambsq" %in% class(obj)))) {
+    #later it could be a option of package:
+    warning("column sq contains at least one amb sequence - not all functions can handle that", call. = FALSE)
   }
   object <- tibble(name = name, sq = sq)
-  class(object) <- c("sqtibble", class(object))
+  class(object) <- c("sqtbl", class(object))
   object
+}
+
+#'@importFrom tibble has_name
+#'@importFrom tibble validate_tibble
+validate_sqtibble <- function(object) {
+  if (!"sqtbl" %in% class(object)) {
+    stop("object doesn't inherit class 'sqtbl'")
+  } 
+  if (!"tbl" %in% class(object)) {
+    stop("object doesn't inherit class 'tbl'")
+  } 
+  validate_tibble(object)
+  if (!has_name(object, "name")) {
+    stop("there's no column named 'name'")
+  }
+  if (!has_name(object, "sq")) {
+    stop("there's no column named 'sq'")
+  }
+  if (!is.character(object$name)) {
+    stop("column 'name' is not a character column")
+  }
+  if (!is.list(object$sq)) {
+    stop("column 'sq' is not a list")
+  }
+  sapply(object$sq, validate_sq)
+  invisible(object)
 }
