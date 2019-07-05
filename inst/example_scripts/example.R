4aded1ade9fbe991565f7f8e31850fe932b2f264
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-05 12:35:18 +0200

    modified examples

--- inst/example_scripts/example.R
+++ inst/example_scripts/example.R
@@ -1,4 +1,4 @@
-#constructing and validating sq
+####constructing and validating sq
 
 sq_1 <- construct_sq("aACccDaaa")
 sq_2 <- construct_aasq("PmHe")
@@ -20,12 +20,12 @@ tidysq:::validate_aasq(sq_6)
 sq_7 <- construct_sq("asatgamktr", type = "nuc")
 tidysq:::validate_nucsq(sq_7)
 
-#constructing and validating sqtibble
+####constructing and validating sqtibble
 
 #at ex_tb1 and ex_tb3 should be warnings
-sqtbl_1 <- tidysq:::construct_sqtibble(c("name2", "name3", "name4"), list(sq_2, sq_3, sq_4))
-sqtbl_2 <- tidysq:::construct_sqtibble(c("name2", "name3"), list(sq_2, sq_3))
-sqtbl_3 <- tidysq:::construct_sqtibble(c("name1", "name3", "name4"), list(sq_1, sq_3, sq_4))
+sqtbl_1 <- construct_sqtibble(c("name2", "name3", "name4"), list(sq_2, sq_3, sq_4))
+sqtbl_2 <- construct_sqtibble(c("name2", "name3"), list(sq_2, sq_3))
+sqtbl_3 <- construct_sqtibble(c("name1", "name3", "name4"), list(sq_1, sq_3, sq_4))
 
 tidysq:::validate_sqtibble(sqtbl_1)
 tidysq:::validate_sqtibble(sqtbl_2)
@@ -34,8 +34,12 @@ tidysq:::validate_sqtibble(sqtbl_3)
 sqtbl_4 <- read_fasta("inst/example_aa.fasta", type = "aa")
 tidysq:::validate_sqtibble(sqtbl_4)
 
+####main testing examles
+
 sqtbl_aa <- read_fasta("inst/small_example_aa.fasta", type = "aa")
 sqtbl_nuc <- read_fasta("inst/small_example_nuc.fasta", type = "nuc")
+
+####testing simple functions
 
 clean(sqtbl_aa)
 clean(sqtbl_aa, only_elements = TRUE)
@@ -46,6 +50,7 @@ clean(sqtbl_nuc, only_elements = TRUE)
 reverse(sqtbl_aa)
 reverse(sqtbl_nuc)
 
+#third example should give warning
 bite(sqtbl_aa, 1:3)
 bite(sqtbl_nuc, 1:5)
 bite(sqtbl_4, 1:20)
