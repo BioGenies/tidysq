db5dc917e7af8af7ae62cc435102c8788a679813
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-05 11:19:53 +0200

    added option that allows colorful display; changed sqtibble sum

--- R/displaying.R
+++ R/displaying.R
@@ -1,3 +1,9 @@
+#' @exportMethod tbl_sum sqtbl
+#' @export
+tbl_sum.sqtbl <- function(x) {
+  c("A sqtibble" = dim_desc(x))
+}
+
 #' @importFrom pillar type_sum
 #' @exportMethod type_sum sqcol
 #' @export
@@ -13,6 +19,9 @@ type_sum.sqcol <- function(x) {
   ret
 }
 
+#' @importFrom crayon col_nchar
+#' @importFrom crayon green
+#' @importFrom crayon silver
 #' @importFrom pillar pillar_shaft
 #' @importFrom pillar new_pillar_shaft
 #' @importFrom pillar get_max_extent
@@ -20,23 +29,29 @@ type_sum.sqcol <- function(x) {
 #' @exportMethod pillar_shaft sqcol
 #' @export
 pillar_shaft.sqcol <- function(x, ...) {
-  x <- as.character(sapply(x, function(i) paste(as.character(ifelse(is.na(i), "*", as.character(i))), collapse = "")))
-  # # x <- format(x, formatter = sqcol_formatter)
-  # inds_too_long <- (nchar(x) > 20)
-  # x[inds_too_long] <- paste0(strtrim(x[inds_too_long], 17), "...")
+  x <- if (get_color_opt()) {
+    as.character(lapply(x, function(i) paste(as.character(ifelse(is.na(i), silver("*"), green(as.character(i)))), collapse = "")))
+  } else {
+    as.character(lapply(x, function(i) paste(as.character(ifelse(is.na(i), "*", as.character(i))), collapse = "")))
+  }
+    
   longest_str <- get_max_extent(x)
   min_str_width <- ifelse(longest_str >= 6, 6, longest_str)
   
   opt <- get_print_length()
   
   new_pillar_shaft(x,
-                   width = min(longest_str + nchar(longest_str) + 3, 
-                               opt + nchar(longest_str) + 6),
-                   min_width = nchar(longest_str) + min_str_width + 3,
+                   width = min(longest_str + col_nchar(longest_str) + 3, 
+                               opt + col_nchar(longest_str) + 6),
+                   min_width = col_nchar(longest_str) + min_str_width + 3,
                    class = "pillar_shaft_sqcol",
                    align = "left")
 }
 
+#' @importFrom crayon col_nchar
+#' @importFrom crayon blue
+#' @importFrom crayon silver
+#' @importFrom crayon col_substring
 #' @importFrom pillar new_ornament
 #' @exportMethod format pillar_shaft_sqcol
 #' @export
@@ -44,20 +59,23 @@ format.pillar_shaft_sqcol <- function(x, width, ...) {
   if (width < attr(x, "min_width")) {
     stop("need at least width ", attr(x, "min_width"), ", requested ", 
          width, ".", call. = FALSE)
-  }
+  } 
   
-  seq_lengths <- nchar(x)
+  s_opt <- if (get_color_opt()) silver else identity
+  b_opt <- if (get_color_opt()) blue else identity
+  
+  seq_lengths <- col_nchar(x)
   nums_lengths <- nchar(seq_lengths)
   available_seq_space <- width - nums_lengths - 3
   inds_need_dots <- available_seq_space < seq_lengths
   available_seq_space[inds_need_dots] <- available_seq_space[inds_need_dots] - 3
-  row <- strtrim(x, available_seq_space)
-  row[inds_need_dots] <- paste0(row[inds_need_dots], "...")
+  row <- col_substring(x, 1, available_seq_space)
+  row[inds_need_dots] <- paste0(row[inds_need_dots], s_opt("..."))
   whitespace_lenghts <- available_seq_space - seq_lengths
   whitespace_lenghts[whitespace_lenghts < 0] <- 0
   whitespace_vec <- sapply(whitespace_lenghts, 
                            function(n) paste(rep(" ", n), collapse = ""))
-  row <- paste0(row, whitespace_vec," [", seq_lengths, "]")
+  row <- paste0(row, whitespace_vec, b_opt(" ["), b_opt(seq_lengths), b_opt("]"))
   
   new_ornament(row, width = width, align = "left")
 }
@@ -65,5 +83,11 @@ format.pillar_shaft_sqcol <- function(x, width, ...) {
 get_print_length <- function() {
   opt <- getOption("tidysq_max_sq_print_width")
   
-  ifelse (!is.null(opt) || !is.numeric(opt) || ! (opt > 0), opt, 15)
+  ifelse (is.null(opt) || !is.numeric(opt) || is.na(opt) || is.nan(opt) || is.infinite(opt) || !(opt > 0), 15, opt)
+}
+
+get_color_opt <- function() {
+  opt <- getOption("tidysq_colorful_sq_print")
+  
+  ifelse (is.null(opt) || !is.logical(opt) || is.na(opt), FALSE, opt)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,8 @@
   prev_options <- options()
   
   new_options <- list(
-    tidysq_max_sq_print_width = 15
+    tidysq_max_sq_print_width = 15,
+    tidysq_colorful_sq_print = TRUE
   )
   
   toset <- !(names(new_options) %in% names(prev_options))
