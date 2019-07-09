46885b9d9881b49d5d0b93078d852f8b44145015
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-09 23:56:01 +0200

    reworked and get_invalid_levels (get_invalid_letters)

---
+++ R/get_invalid_letters.R
@@ -0,0 +1,15 @@
+#' @export
+get_invalid_letters <- function(sq, dest_type) {
+  validate_sq(sq)
+  if (missing(dest_type) || 
+     !(dest_type %in% c("ami", "nuc"))) {
+    stop("'dest_type' should be either 'ami' or 'nuc'")
+  }
+  
+  alph <- .get_alph(sq)
+  dest_alph <- if (dest_type == "ami") aminoacids_df[, "one"] else nucleotides_df[, "one"]
+  dest_alph <- c(dest_alph, tolower(dest_alph))
+  
+  inv_levels <- lapply(sq, function(s) setdiff(alph[s], dest_alph))
+  inv_levels
+}
