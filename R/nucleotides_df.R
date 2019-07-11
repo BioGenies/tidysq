cf51f410c4cba1149c8c1701d1eda7a97781488b
fpietluch <fpietluch@gmail.com>
2019-07-11 22:05:17 +0200

    added nucleotides_df doc

---
+++ R/nucleotides_df.R
@@ -0,0 +1,17 @@
+#' Nucleotides abbreviations 
+#' 
+#' A dataset containing the nucleotide letter code and full names od nucleotides according to IUPAC nomenclature
+#' @name nucleotides_df
+#' @docType data 
+#' @format  A data frame with 17 rows and 3 columns:
+#' \describe{
+#'  \item{one}{One-letter codes of nucleotides}
+#'  \item{item}{Full name of the nucleotide}
+#'  \item{amb}{Logical indicating if abbreviation is ambiguous, i.e., matches more than one nucleotide}
+#'  }
+#'  @source IUPAC 
+#'  @keywords datasets
+#'  @examples 
+#'  data(nucleotides_df)
+#'       
+NULL          
