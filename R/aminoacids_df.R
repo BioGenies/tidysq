06141b6c24ec2d7a1ac3f94f05ebae0d99406018
michbur <michalburdukiewicz@gmail.com>
2019-07-13 14:35:21 +0200

    updated source

--- R/aminoacids_df.R
+++ R/aminoacids_df.R
@@ -1,6 +1,7 @@
 #' Amino acid abbreviations
 #' 
-#' A dataset containing the one- and three-letter codes, and full names of amino acids according to the IUPAC nomenclature.
+#' A dataset containing the one- and three-letter codes, and full names of 
+#' amino acids according to the IUPAC nomenclature.
 #' 
 #' @name aminoacids_df
 #' @docType data
@@ -9,9 +10,11 @@
 #'  \item{one}{One-letter codes of amino acids}
 #'  \item{three}{Three-letter codes of amino acids}
 #'  \item{full}{Full name of the amino acid}
-#'  \item{amb}{Logical indicating if abbreviation is ambiguous, i.e., matches more than one amino acid}
+#'  \item{amb}{Logical indicating if abbreviation is ambiguous, i.e., matches 
+#'  more than one amino acid}
 #'  }
-#' @source IUPAC
+#' @source Johnson, A.D. (2010). An extended IUPAC nomenclature code for 
+#' polymorphic nucleic acids. Bioinformatics 26, 1386–1389.
 #' @keywords datasets
 #' @examples 
 #' data(aminoacids_df)
