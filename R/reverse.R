92646b3bd7d74d638319dbabc245b1f89a2c11f1
unknown <ciemnamateria@gmail.com>
2019-07-15 16:55:25 +0200

    added examples of reverse documentation

--- R/reverse.R
+++ R/reverse.R
@@ -1,15 +1,27 @@
 #' Reverse sequence
 #' 
-#' Reverse given list of sequences. 
+#' Reverse given list of sequences.
 #' 
-#' @param sq sq object
+#' @param sq \code{\link{sq}} object
 #' 
-#' @return reversed sq object (with preserved type)
+#' @return \code{\link{sq}} object of the same type as input sq, where each element is reversed.
 #' 
 #' @examples 
+#' # Reverse just one sequence
 #' reverse(construct_sq("ACTAGAGTGATAGA", type = "nuc"))
+#' 
+#' # Reverse list of sequences
 #' reverse(construct_sq(c("fafasfasfFSA", "ygagayagfa", "adsDaf"), type = "ami"))
+#' 
+#' # Reverse list of uncleaned sequences
+#' reverse(construct_sq(c("PASJIFEHF", "hvfisxxx", "xxxer")))
+#' 
+#' # Reverse list of cleaned sequences - only_elements = FALSE
+#' reverse(clean(construct_sq(c("fafasfasfFSA", "ygagayagfa", "adxxaf", "xxx"), type = "ami")))
 #'
+#' # Reverse list of cleaned sequences - only_elements = TRUE
+#' reverse(clean(construct_sq(c("fafasfasfFSA", "ygagayagfa", "adxxaf", "xxx"), type = "ami"), only_elements = TRUE))
+#' 
 #' @export
 reverse <- function(sq) {
   validate_sq(sq)
