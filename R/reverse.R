b877804b4d2c522171a96e59d36feee98b60c134
unknown <ciemnamateria@gmail.com>
2019-07-15 17:06:42 +0200

    changed letters size in examples to fit bite

--- R/reverse.R
+++ R/reverse.R
@@ -7,20 +7,22 @@
 #' @return \code{\link{sq}} object of the same type as input sq, where each element is reversed.
 #' 
 #' @examples 
-#' # Reverse just one sequence
+#' # reverse just one sequence:
 #' reverse(construct_sq("ACTAGAGTGATAGA", type = "nuc"))
 #' 
-#' # Reverse list of sequences
+#' # reverse list of sequences:
 #' reverse(construct_sq(c("fafasfasfFSA", "ygagayagfa", "adsDaf"), type = "ami"))
 #' 
-#' # Reverse list of uncleaned sequences
+#' # reverse list of uncleaned sequences:
 #' reverse(construct_sq(c("PASJIFEHF", "hvfisxxx", "xxxer")))
 #' 
-#' # Reverse list of cleaned sequences - only_elements = FALSE
+#' # reverse list of cleaned sequences - only_elements = FALSE:
 #' reverse(clean(construct_sq(c("fafasfasfFSA", "ygagayagfa", "adxxaf", "xxx"), type = "ami")))
 #'
-#' # Reverse list of cleaned sequences - only_elements = TRUE
+#' # reverse list of cleaned sequences - only_elements = TRUE:
 #' reverse(clean(construct_sq(c("fafasfasfFSA", "ygagayagfa", "adxxaf", "xxx"), type = "ami"), only_elements = TRUE))
+#' 
+#' @seealso sq
 #' 
 #' @export
 reverse <- function(sq) {
