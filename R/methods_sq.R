5e3b8ae4180ab75ddc40ff33ed064bb1e49cc05b
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-25 10:08:04 +0200

    added missing function

--- R/methods_sq.R
+++ R/methods_sq.R
@@ -13,7 +13,18 @@
 #' @export
 as.character.sq <- function(x, ...) {
   alph <- .get_alph(x)
-  sapply(.debitify_sq(x, alph), function(s) paste(ifelse(is.na(s), "*", s), collapse = ""))
+  if (.is_no_check_mode()) {
+    type <- .get_sq_type(x)
+    if (type == "nuc") {
+      if (.is_cleaned(x)) {
+        .debitify_sq_cnuc(x)
+      } else {
+        stop("not implemented!")
+      }
+    } else {
+      stop("not implemented!")
+    }
+  } else sapply(.debitify_sq(x, alph), function(s) paste(ifelse(is.na(s), "*", s), collapse = ""))
 }
 
 #' @exportMethod is sq
