aab55c6a5450e6d4c808655795d701113b9b38c2
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-29 20:02:27 +0200

    changed once again low-level interface of bitification

--- R/util_bit.R
+++ R/util_bit.R
@@ -1,82 +1,56 @@
-.bitify_sq <- function(sq, type = NULL, is_clean = NULL, alph = NULL) {
+.bitify_sq <- function(sq, alph) {
   if (is.numeric(sq[[1]])) 
-    t_fun <- rawize
+    pack_fun <- pack_ints
   else if (any(lengths(sq) > 1)) 
-    t_fun <- function(x) charToRaw(paste(x, collapse = ""))
-  else t_fun <- charToRaw
-  if (is.null(alph)) {
-    pack_fun <- if (type == "ami") {
-      if (is_clean) pack_cami else pack_ami
-    } else if (type == "nuc") {
-      if (is_clean) pack_cnuc else pack_nuc
-    }
-    lapply(sq, function(s) {
-      pack_fun(t_fun(s))
-    })
-  } else {
-    alph_size <- .get_alph_size(alph)
-    alph <- .rawize_alph(alph)
-    lapply(sq, function(s) {
-      pack_unt(t_fun(s), alph, alph_size)
-    })
-  }
-}
-
-.debitify_sq <- function(sq, to = "char") {
-  if (to == "char") 
-    t_fun <- rawToChar
-  else if (to == "int") 
-    t_fun <- integerize
-  else if (to == "chars") 
-    t_fun <- function(x) strsplit(rawToChar(x), "")[[1]]
+    pack_fun <- pack_chars
+  else pack_fun <- function(s, alph) pack_string(charToRaw(s), alph)
   
-  na_char <- .get_na_char()
-  type <- .get_sq_type(sq)
-  if (type %in% c("ami", "nuc")) {
-    is_clean <- .is_cleaned(sq)
-    unpack_fun <- if (type == "ami") {
-      if (is_clean) unpack_cami else unpack_ami
-    } else if (type == "nuc") {
-      if (is_clean) unpack_cnuc else unpack_nuc
-    }
-    sapply(sq, function(s) t_fun(unpack_fun(s, na_char)))
-  } else {
-    alph <- charToRaw(paste(.get_alph(sq), collapse = ""))
-    alph_size <- .get_alph_size(alph)
-    sapply(sq, function(s) t_fun(unpack_unt(s, na_char, alph, alph_size)))
-  }
-}
-
-
-.recode_sq <- function(sq, alph, new_alph, letters_fun) {
-  na_val <- .get_na_val(alph)
-  new_na_val <- .get_na_val(new_alph)
   alph_size <- .get_alph_size(alph)
-  new_alph_size <- .get_alph_size(new_alph)
-  alph <- .rawize_alph(alph)
-  new_alph <- .rawize_alph(new_alph)
   lapply(sq, function(s) {
-    s <- repack(s, na_char, alph, 
-                new_alph, 
-                alph_size, new_alph_size, 
-                letters_fun)
+    pack_fun(s, alph)
   })
 }
 
-.apply_sq <- function(sq, intern_form, fun) {
-  if (intern_form == "char") {
-    t_fun <- rawToChar
-  } else if (intern_form == "int") {
-    t_fun <- integerize
-  } else if (intern_form == "chars") {
-    t_fun <- function(x) strsplit(rawToChar(x), "")[[1]]
-  } 
+.nc_bitify_sq <- function(sq, type, is_clean) {
+  if      (type == "ami" &&  is_clean) pack_fun <- nc_pack_cami
+  else if (type == "ami" && !is_clean) pack_fun <- nc_pack_ami
+  else if (type == "nuc" &&  is_clean) pack_fun <- nc_pack_cnuc
+  else if (type == "nuc" && !is_clean) pack_fun <- nc_pack_nuc
+  
+  lapply(sq, function(s) pack_fun(charToRaw(s)))
+}
+
+.debitify_sq <- function(sq, to) {
   alph <- .get_alph(sq)
-  alph_size <- .get_alph_size(alph)
-  na_char <- .get_na_char()
-  alph <- .rawize_alph(alph)
+  if (to == "char") 
+    unpack_fun <- function(s) unpack_chars(s, alph, .get_na_char())
+  else if (to == "int") 
+    unpack_fun <- function(s) unpack_ints(s, .get_alph_size(alph))
+  else if (to == "string") 
+    unpack_fun <- function(s) rawToChar(unpack_string(s, alph, .get_na_char()))
+  
+  sapply(sq, function(s) unpack_fun(s))
+}
+
+.apply_sq <- function(sq, ex_form, im_form, fun) {
+  alph <- .get_alph(sq)
+  if (ex_form == "char") 
+    unpack_fun <- function(s) unpack_chars(s, alph, .get_na_char())
+  else if (ex_form == "int") 
+    unpack_fun <- function(s) unpack_ints(s, .get_alph_size(alph))
+  else if (ex_form == "string") 
+    unpack_fun <- function(x) rawToChar(unpack_string(s, alph, .get_na_char()))
+ 
+  if (im_form == "char")
+    pack_fun <- function(s) pack_chars(s, alph)
+  else if (im_form == "int")
+    pack_fun <- function(s) pack_ints(s, .get_alph_size(alph))
+  else if (im_form == "string")
+    pack_fun <- function(s) pack_string(charToRaw(s), alph)
+  else if (im_form == "none")
+    pack_fun <- identity
+
   lapply(sq, function(s) {
-    s <- t_fun(unpack_unt(s, na_char, alph, alph_size))
-    fun(s)
+    s <- pack_fun(fun(unpack_fun(s)))
   })
 }
