558f9c441e9c9f2c65b62c3d33c84dff06a5929b
jarochi <jaroslaw.chilimoniuk@gmail.com>
2019-07-29 22:31:50 +0200

    desc, det expand

--- R/substitute_letters.R
+++ R/substitute_letters.R
@@ -1,7 +1,10 @@
 #' Substitute default amino acid or nucleic acid alphabet with a custom encoding
 #' 
 #' @description Replace default amino acid or nucleic acid letters in a sequence, 
-#' stored in \code{\link{sq}} object, with a custom encoding.
+#' stored in \code{\link{sq}} object, with a custom encoding. 
+#' Selected letters in the amino acid or nucleic acid sequence are replaced 
+#' by a user-defined symbols.
+#' 
 #' 
 #' @param sq \code{\link{sq}} object.
 #' @param indices \code{encoding} vector of letters to be replaced together with their replacements.
@@ -10,6 +13,32 @@
 #' 
 #' @details Function allows to replace default alphabet encoding with letters desired by the user.
 #' One letter of the alphabet may be replaced by a string of characters.
+#' 
+#' Sometimes one needs to replace default amino/nucleic acid alphabet with custom one. 
+#' Such an example could be the use of simplified amino acid alphabet, which take into account 
+#' different physicochemical properties of amino acids to cluster them into fewer groups.
+#' The simplification preserves the informative character of the alphabet while reducing the 
+#' number of required operations when using it in futher steps of pipeline, such as machine learning.
+#' 
+#' 
+#' #' @details Amino acids and nucleic acid sequences are represented as \code{\link{sq}} 
+#' object in the \code{\link{tidysq}} package. Often one needs to get only a 
+#' single letter or the sequence of a defined range from the original sequences. 
+#' A subsequence is a sequence that can be derived from the original sequence 
+#' by trimming some elements (letters) without changing the order of the 
+#' remaining elements. To obtain a subsequence from each sequence contained in 
+#' the \code{\link{sq}} object with the same indices. This is for example 
+#' useful to extract an user-defined region from a sequence. 
+#' 
+#' The usage of \code{bite} follows the normal R conventions. For details 
+#' refer to the R documentation (see 
+#' \url{https://cran.r-project.org/doc/manuals/r-release/R-intro.html#Index-vectors}). 
+#' Extracting indices not present in the sequence results in introducing 
+#' \code{\link{NA}} (‘Not Available’ / Missing Values) values. 
+#' Information about it is printed on console depending on value of option 
+#' 'tidysq_bite_na_action' - it can be either a warning (default), error, 
+#' message or no information (you can check details in \code{\link{sq-options})}. 
+#' \code{NA} values can be removed by using \code{\link{remove_na}} function.
 #' 
 #' @examples 
 #' # Creating object, called sq to work on:
