015aa86ead11348746863e8f28bf5a99f1052a46
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-07-30 11:50:14 +0200

    added  tests template

--- tests/testthat/test_constructing.R
+++ tests/testthat/test_constructing.R
@@ -1,6 +1,51 @@
-context("Constructing sq objects")
-
-#
-test_that("non-ambiguous nucleotides", {
-  construct_sq("ATG")
+test_that("single short ambiguous nucleotides sequences constructing", {
+  expect_identical(construct_sq("ATGN"),
+                   structure(list(as.raw(c(0x81, 0x0c, 0x08))), 
+                             alphabet = c("A", "C", "G", "T", "U", "W", 
+                                          "S", "M", "K", "R", "Y", "B", 
+                                          "D", "H", "V", "N", "-"), 
+                             class = c("nucsq", "sq")))
 })
+test_that("multiple short ambiguous nucleotides sequences constructing", {})
+test_that("single medium-length ambiguous nucleotides sequences constructing", {})
+test_that("multiple medium-length ambiguous nucleotides sequences constructing", {})
+test_that("single long ambiguous nucleotides sequences constructing", {})
+test_that("multiple long ambiguous nucleotides sequences constructing", {})
+test_that("single short non-ambiguous nucleotides sequences constructing", {
+  expect_identical(construct_sq("ATG"),
+                   structure(list(as.raw(c(0xe1, 0x00))), 
+                             alphabet = c("A", "C", "G", "T", "U", "-"), 
+                             class = c("clnsq", "nucsq", "sq")))
+})
+test_that("multiple short non-ambiguous nucleotides sequences constructing", {})
+test_that("single medium-length non-ambiguous nucleotides sequences constructing", {})
+test_that("multiple medium-length non-ambiguous nucleotides sequences constructing", {})
+test_that("single long non-ambiguous nucleotides sequences constructing", {})
+test_that("multiple long non-ambiguous nucleotides sequences constructing", {})
+test_that("single short ambiguous aminoacids sequences constructing", {
+  expect_identical(construct_sq("ATGN"),
+                   structure(list(as.raw(c(0x81, 0x0c, 0x08))), 
+                             alphabet = c("A", "C", "G", "T", "U", "W", 
+                                          "S", "M", "K", "R", "Y", "B", 
+                                          "D", "H", "V", "N", "-"), 
+                             class = c("nucsq", "sq")))
+})
+test_that("multiple short ambiguous aminoacids sequences constructing", {})
+test_that("single medium-length ambiguous aminoacids sequences constructing", {})
+test_that("multiple medium-length ambiguous aminoacids sequences constructing", {})
+test_that("single long ambiguous aminoacids sequences constructing", {})
+test_that("multiple long ambiguous aminoacids sequences constructing", {})
+test_that("single short non-ambiguous aminoacids sequences constructing", {
+  expect_identical(construct_sq("PQKH"),
+                   structure(list(as.raw(c(0xcd, 0xa5, 0x03))), 
+                             alphabet = c("A", "C", "D", "E", "F", "G", 
+                                          "H", "I", "K", "L", "M", "N", 
+                                          "P", "Q", "R", "S", "T", "V", 
+                                          "W", "Y", "-"), 
+                             class = c("clnsq", "amisq", "sq")))
+})
+test_that("multiple short non-ambiguous aminoacids sequences constructing", {})
+test_that("single medium-length non-ambiguous aminoacids sequences constructing", {})
+test_that("multiple medium-length non-ambiguous aminoacids sequences constructing", {})
+test_that("single long non-ambiguous aminoacids sequences constructing", {})
+test_that("multiple long non-ambiguous aminoacids sequences constructing", {})
