feb0e546b070efc4e8884d2a85c0d28406e916e5
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-08-02 13:23:45 +0200

    moved get_invalid_letters and is_null_sq to support_funs file

--- R/support_funs.R
+++ R/support_funs.R
@@ -15,3 +15,27 @@ get_sq_lengths <- function(sq) {
   validate_sq(sq)
   .get_lens(sq)
 }
+
+#' @export
+is_null_sq <- function(sq) {
+  validate_sq(sq)
+  unlist(.apply_sq(sq, "char", "none", function(s) length(s) == 0 || 
+                     length(s) == 1 && identical(s, "")))
+}
+
+#' @export
+get_invalid_letters <- function(sq, dest_type) {
+  validate_sq(sq)
+  if (missing(dest_type) || 
+     !(dest_type %in% c("ami", "nuc"))) {
+    stop("'dest_type' should be either 'ami' or 'nuc'")
+  }
+  
+  alph <- .get_alph(sq)
+  alph_size <- .get_alph_size(alph)
+  na_char <- .get_na_char()
+  dest_alph <- .get_standard_alph(dest_type, FALSE)
+  dest_alph <- c(dest_alph, tolower(dest_alph))
+  
+  .apply_sq(sq, "char", "none", function(s) setdiff(s, dest_alph))
+}
