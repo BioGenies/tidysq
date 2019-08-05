c1c14f50a096184d9931f745ca4734e74252b5b1
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-08-05 12:51:06 +0200

    added templates for bite tests

---
+++ tests/testthat/test_bite.R
@@ -0,0 +1,6 @@
+test_that("error on non-numeric 'indices' argument", {})
+
+test_that("biting sq with positive indices - no reaching outside range", {})
+test_that("biting sq with positive indices - reaching outside range", {})
+test_that("biting sq with negative indices - no reaching outside range", {})
+test_that("biting sq with negative indices - reaching outside range", {})
