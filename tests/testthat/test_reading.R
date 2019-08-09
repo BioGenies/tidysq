78db382d7fd277f212dcfd40c38237635b8131c4
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-08-09 15:03:33 +0200

    added template for read_fasta tests and another sample file

---
+++ tests/testthat/test_reading.R
@@ -0,0 +1,6 @@
+test_that("reading fasta file with clean aminoacid sequences with type and is_clean uspecified", {})
+test_that("reading fasta file with ambiguous aminoacid sequences with type and is_clean uspecified", {})
+test_that("reading fasta file with clean nucleotides sequences with type and is_clean uspecified", {})
+test_that("reading fasta file with ambiguous nucleotides sequences with type and is_clean uspecified", {})
+
+test_that("reading fasta file with non-standard element", {})
