96daeeb2c5b18f1f554c84f36aab85326c7821e9
unknown <ciemnamateria@gmail.com>
2019-08-14 12:02:01 +0200

    more tests for bite

--- tests/testthat/test_bite.R
+++ tests/testthat/test_bite.R
@@ -2,35 +2,57 @@ sq_ami_cln <- clean(construct_sq(c("MIAANYTWIL","TIAALGNIIYRAIE",
                         "NYERTGHLI", "MAYXXXIALN"), type = "ami"), only_elements = TRUE)
 
 sq_ami_cln_short <- clean(construct_sq(c("MI","TI", "NY", "MA"), type = "ami"), only_elements = TRUE)
+sq_ami_cln_short_2 <- clean(construct_sq(c("IAA","IAA", "YER", "AYI"), type = "ami"), only_elements = TRUE)
 
 sq_ami_cln_cut <- clean(construct_sq(c("IAANYTWIL","IAALGNIIYRAIE", 
                                        "YERTGHLI", "AYXXXIALN"), type = "ami"), only_elements = TRUE)
 
-sq_ami_cln_na <- structure(list(as.raw(c(0xff, 0xff, 0xff, 0xff, 0xff)), 
-                                as.raw(c(0x88, 0xfc, 0xff, 0xff, 0xff)), 
-                                as.raw(c(0xff, 0xff, 0xff, 0xff, 0xff)), 
-                                as.raw(c(0xff, 0xff, 0xff, 0xff, 0xff))), 
+sq_ami_cln_cut_2 <- clean(construct_sq(c("ANYTWIL","ALGNIIYRAIE", 
+                                       "RTGHLI", "IALN"), type = "ami"), only_elements = TRUE)
+
+sq_ami_cln_na <- structure(list(as.raw(c(0xff, 0xff, 0xff, 0xff, 0xff)), as.raw(c(0x88, 0xfc, 0xff, 0xff, 0xff)), 
+                                as.raw(c(0xff, 0xff, 0xff, 0xff, 0xff)), as.raw(c(0xff, 0xff, 0xff, 0xff, 0xff))), 
                            class = c("clnsq", "amisq", "sq"), 
                            alphabet = c("A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y", "-", "*"))
 
+sq_ami_cln_na_2 <- structure(list(as.raw(c(0xff, 0x7f)), as.raw(c(0x01, 0x11)), 
+                                  as.raw(c(0xff, 0x7f)), as.raw(c(0xff, 0x7f))), 
+                             class = c("clnsq", "amisq", "sq"), 
+                             alphabet = c("A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y", "-", "*"))
+
 #############
 
-test_that("error on non-numeric 'indices' argument", 
-          expect_error(bite(sq_ami_cln, "n")))
+test_that("error on non-numeric 'indices' argument", {
+  expect_error(bite(sq_ami_cln, "n"))
+  expect_error(bite(sq_ami_cln, NA))
+})
 
-test_that("biting sq with positive indices - no reaching outside range", 
-          expect_equal(bite(sq_ami_cln, 1:2),
-                       sq_ami_cln_short))
+test_that("biting sq with positive indices - no reaching outside range", {
+  expect_equal(bite(sq_ami_cln, 1:2),
+               sq_ami_cln_short)
+  expect_equal(bite(sq_ami_cln, 2:4),
+               sq_ami_cln_short_2)
+})
 
-test_that("biting sq with positive indices - reaching outside range", 
-          expect_equal(bite(sq_ami_cln, 13:20), 
-                       sq_ami_cln_na))
+test_that("biting sq with positive indices - reaching outside range", {
+  expect_equal(bite(sq_ami_cln, 13:20), 
+               sq_ami_cln_na)
+  expect_equal(bite(sq_ami_cln, 12:14), 
+               sq_ami_cln_na_2)
+})
 
-test_that("biting sq with negative indices - no reaching outside range", 
-          expect_equal(bite(sq_ami_cln, -1),
-                       sq_ami_cln_cut))
+test_that("biting sq with negative indices - no reaching outside range", {
+  expect_equal(bite(sq_ami_cln, -1),
+               sq_ami_cln_cut)
+  expect_equal(bite(sq_ami_cln, -1:-3),
+               sq_ami_cln_cut_2)
+})
 
-test_that("biting sq with negative indices - reaching outside range", 
-          expect_equal(bite(sq_ami_cln, -15:-20),
-                       sq_ami_cln))
+test_that("biting sq with negative indices - reaching outside range", {
+  expect_equal(bite(sq_ami_cln, -15:-20),
+               sq_ami_cln)
+  expect_equal(bite(sq_ami_cln, -17:-18),
+               sq_ami_cln)
+})
+          
 
