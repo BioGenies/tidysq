c054fe5b9fd8193490b4534b7593dc892f729602
unknown <ciemnamateria@gmail.com>
2019-08-14 12:10:01 +0200

    more tests for remove_na

--- tests/testthat/test_remove_na.R
+++ tests/testthat/test_remove_na.R
@@ -1,10 +1,21 @@
-sq_ami <- bite(construct_sq(c("MIAANYTWIL", "", "TIAALGNIIYRAIE", "NYERTGHLI", "MAYXXXIALN")), 1:14)
+sq_ami <- bite(construct_sq(c("MIAANYTWIL", "", "TIAALGNIIYRAIE", "NYERTGHLI", "MAYXXXIALN"), type = "ami"), 1:14)
 sq_ami_removed_na_elements <- construct_sq(c("MIAANYTWIL", "", "TIAALGNIIYRAIE", "NYERTGHLI", "MAYXXXIALN"))
 sq_ami_removed_na <- construct_sq(c("", "", "TIAALGNIIYRAIE", "", ""))
+sq_nuc <- bite(construct_sq(c("ATGCAGGA", "GACCGAACGAN", "TGACGAGCTTA", "ACTNNAGCN"), type = "nuc"), 1:11)
+sq_nuc_removed_na_elements <- construct_sq(c("ATGCAGGA", "GACCGAACGAN", "TGACGAGCTTA", "ACTNNAGCN"), type = "nuc")
+sq_nuc_removed_na <- construct_sq(c("", "", "TGACGAGCTTA", ""), type = "nuc")
 
-test_that("removing NA with only_elements = TRUE", 
-          expect_equal(remove_na(sq_ami, only_elements = TRUE),
-                       sq_ami_removed_na_elements))
-test_that("removing NA with only_elements = FALSE", 
-          expect_equal(clean(remove_na(sq_ami, only_elements = FALSE)),
-                       sq_ami_removed_na))
+test_that("removing NA with only_elements = TRUE", {
+  expect_equal(remove_na(sq_ami, only_elements = TRUE),
+               sq_ami_removed_na_elements)
+  expect_equal(remove_na(sq_nuc, only_elements = TRUE),
+               sq_nuc_removed_na_elements)
+})
+          
+test_that("removing NA with only_elements = FALSE", {
+  expect_equal(clean(remove_na(sq_ami, only_elements = FALSE)),
+               sq_ami_removed_na)
+  expect_equal(clean(remove_na(sq_nuc, only_elements = FALSE)),
+               sq_nuc_removed_na)
+})
+          
