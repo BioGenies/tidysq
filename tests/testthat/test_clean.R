21e7575ffd783d0f81c635872de474344475d884
Mateusz <matibakala@gmail.com>
2020-07-09 00:25:54 +0200

    all tests but test_construct.R updated

--- tests/testthat/test_clean.R
+++ tests/testthat/test_clean.R
@@ -1,9 +1,9 @@
 sq_ami <- construct_sq(c("MIAANYTWIL","TIAALGNIIYRAIE", "NYERTGHLI", "MAYXXXIALN"), type = "ami")
 sq_ami_cln <- construct_sq(c("MIAANYTWIL","TIAALGNIIYRAIE", "NYERTGHLI", ""), type = "ami")
 sq_ami_cln_elements <- construct_sq(c("MIAANYTWIL","TIAALGNIIYRAIE", "NYERTGHLI", "MAYIALN"), type = "ami")
-sq_nuc <- construct_sq(c("ATGCAGGA", "GACCGAACGAN", "TGACGAGCTTA", "ACTNNAGCN"), type = "nuc")
-sq_nuc_cln <- construct_sq(c("ATGCAGGA", "", "TGACGAGCTTA", ""), type = "nuc")
-sq_nuc_cln_elements <- construct_sq(c("ATGCAGGA", "GACCGAACGA", "TGACGAGCTTA", "ACTAGC"), type = "nuc")
+sq_dna <- construct_sq(c("ATGCAGGA", "GACCGAACGAN", "TGACGAGCTTA", "ACTNNAGCN"), type = "dna")
+sq_dna_cln <- construct_sq(c("ATGCAGGA", "", "TGACGAGCTTA", ""), type = "dna")
+sq_dna_cln_elements <- construct_sq(c("ATGCAGGA", "GACCGAACGA", "TGACGAGCTTA", "ACTAGC"), type = "dna")
 sq_rna <- construct_sq(c("UGGCGNNGBV", "ACGGUUUCGUU", "UBBGDGAACG", "GGCUCGACAGACUGC"), type = "rna")
 sq_rna_cln <- construct_sq(c("", "ACGGUUUCGUU", "", "GGCUCGACAGACUGC"), type = "rna")
 sq_rna_cln_elements <- construct_sq(c("UGGCGG", "ACGGUUUCGUU", "UGGAACG", "GGCUCGACAGACUGC"), type = "rna")
@@ -12,12 +12,10 @@ test_that("cleaning doesn't affect clean ami sq", {
   expect_equal(clean(sq_ami_cln),
                sq_ami_cln)
 })
-
-test_that("cleaning doesn't affect clean nuc sq", {
-  expect_equal(clean(sq_nuc_cln),
-               sq_nuc_cln)
+test_that("cleaning doesn't affect clean dna sq", {
+  expect_equal(clean(sq_dna_cln),
+               sq_dna_cln)
 })
-
 test_that("cleaning doesn't affect clean rna sq", {
   expect_equal(clean(sq_rna_cln),
                sq_rna_cln)
@@ -27,27 +25,23 @@ test_that("cleaning ami sq with parameter only_elements = FALSE", {
   expect_equal(clean(sq_ami, only_elements = FALSE),
                sq_ami_cln)
 })
-
-test_that("cleaning nuc sq with parameter only_elements = TRUE", {
-  expect_equal(clean(sq_ami, only_elements = TRUE),
-               sq_ami_cln_elements)
+test_that("cleaning dna sq with parameter only_elements = FALSE", {
+  expect_equal(clean(sq_dna, only_elements = FALSE),
+               sq_dna_cln)
 })
-
-test_that("cleaning ami sq with parameter only_elements = FALSE", {
-  expect_equal(clean(sq_nuc, only_elements = FALSE),
-               sq_nuc_cln)
-})
-
-test_that("cleaning nuc sq with parameter only_elements = TRUE", {
-  expect_equal(clean(sq_nuc, only_elements = TRUE),
-               sq_nuc_cln_elements)
-})
-
 test_that("cleaning rna sq with parameter only_elements = FALSE", {
   expect_equal(clean(sq_rna, only_elements = FALSE),
                sq_rna_cln)
 })
 
+test_that("cleaning ami sq with parameter only_elements = TRUE", {
+  expect_equal(clean(sq_ami, only_elements = TRUE),
+               sq_ami_cln_elements)
+})
+test_that("cleaning dna sq with parameter only_elements = TRUE", {
+  expect_equal(clean(sq_dna, only_elements = TRUE),
+               sq_dna_cln_elements)
+})
 test_that("cleaning rna sq with parameter only_elements = TRUE", {
   expect_equal(clean(sq_rna, only_elements = TRUE),
                sq_rna_cln_elements)
--- tests/testthat/test_find_motifs.R
+++ tests/testthat/test_find_motifs.R
@@ -1,104 +1,135 @@
 sq_ami <- construct_sq(c("AGNTYIB", "MATEGILI", "MIPADHICA"), type = 'ami')
-sq_nuc <- construct_sq(c("CTGAATGCAGT", "ATGCCGT", "CAGACCANN"), type = 'nuc')
+sq_dna <- construct_sq(c("CTGAATGCAGT", "ATGCCGT", "CAGACCANN"), type = 'dna')
+sq_rna <- construct_sq(c("UUACGACUU", "UUAAGCGC", "ACUAAGACCA"), type = 'rna')
 
 test_that("find_motifs detects correctly motif that is single unambiguous amino acid in sequences", {
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "A"))[1],
-               4)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "C"))[1],
-               1)
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "A")[["start"]],
+               c(1, 2, 4, 9))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "C")[["start"]],
+               8)
 })
 
-test_that("find_motifs detects correctly motif that is single unambiguous nucleotide in sequences", {
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "A"))[1],
-               7)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "C"))[1],
-               7)
+test_that("find_motifs detects correctly motif that is single unambiguous DNA nucleotide in sequences", {
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "A")[["start"]],
+               c(4, 5, 9, 1, 2, 4, 7))
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "C")[["start"]],
+               c(1, 8, 4, 5, 1, 5, 6))
+})
+
+test_that("find_motifs detects correctly motif that is single unambiguous RNA nucleotide in sequences", {
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "A")[["start"]],
+               c(3, 6, 3, 4, 1, 4, 5, 7, 10))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "C")[["start"]],
+               c(4, 7, 6, 8, 2, 8, 9))
 })
 
 test_that("find_motifs detects correctly motif that is single ambiguous amino acid in sequences", {
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "B"))[1],
-               3)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "J"))[1],
-               6)
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "B")[["start"]],
+               c(3, 7, 5))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "J")[["start"]],
+               c(6, 6, 7, 8, 2, 7))
 })
 
-test_that("find_motifs detects correctly motif that is single ambiguous nucleotide in sequences", {
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "W"))[1],
-               12)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "K"))[1],
-               11)
+test_that("find_motifs detects correctly motif that is single ambiguous DNA nucleotide in sequences", {
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "W")[["start"]],
+               c(2, 4, 5, 6, 9, 11, 1, 2, 7, 2, 4, 7))
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "K")[["start"]],
+               c(2, 3, 6, 7, 10, 11, 2, 3, 6, 7, 3))
+})
+
+test_that("find_motifs detects correctly motif that is single ambiguous RNA nucleotide in sequences", {
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "B")[["start"]],
+               c(1, 2, 3, 6, 7, 8, 10, 11, 2, 3, 4, 5, 6, 7, 1, 3, 5, 6))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "Y")[["start"]],
+               c(1, 2, 6, 8, 11, 2, 4, 5, 7, 1, 5, 6))
 })
 
 test_that("find_motifs detects correctly leading letters of amino acid sequences using '^'", {
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "^A"))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "^M"))[1],
-               2)
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "^A")[["start"]],
+               c(start = 1))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "^M")[["start"]],
+               c(1, 1))
 })
 
-test_that("find_motifs detects correctly leading letters of nucleotide sequences using '^'", {
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "^C"))[1],
-               2)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "^A"))[1],
-               1)
+test_that("find_motifs detects correctly leading letters of DNA nucleotide sequences using '^'", {
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "^C")[["start"]],
+               c(1, 1))
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "^A")[["start"]],
+               c(start = 1))
+})
+
+test_that("find_motifs detects correctly leading letters of RNA nucleotide sequences using '^'", {
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "^C")[["start"]],
+               numeric(0))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "^A")[["start"]],
+               c(start = 1))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "^UU")[["start"]],
+               c(1, 1))
 })
 
 test_that("find_motifs detects correctly letters at the end of amino acid sequences using '$'", {
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "B$"))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "I$"))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "A$"))[1],
-               1)
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "B$")[["start"]],
+               c(start = 7))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "I$")[["start"]],
+               c(start = 8))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "A$")[["start"]],
+               c(start = 9))
 })
 
-test_that("find_motifs detects correctly letters at the end of nucleotide sequences using '$'", {
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "T$"))[1],
-               2)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "N$"))[1],
-               3)
+test_that("find_motifs detects correctly letters at the end of DNA nucleotide sequences using '$'", {
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "T$")[["start"]],
+               c(11, 7))
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "N$")[["start"]],
+               c(11, 7, 9))
+})
+
+test_that("find_motifs detects correctly letters at the end of RNA nucleotide sequences using '$'", {
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "D$")[["start"]],
+               c(9, 10))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "C$")[["start"]],
+               c(start = 8))
 })
 
 test_that("find_motifs detects correctly multiple-letter motifs in amino acid sequences", {
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "TY"))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "PAD"))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "GILI"))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "RARARARA"))[1],
-               0)
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "TY")[["start"]],
+               c(start = 4))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "PAD")[["start"]],
+               c(start = 3))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "GILI")[["start"]],
+               c(start = 5))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), "RARARARA")[["start"]],
+               numeric(0))
 })
 
 test_that("find_motifs detects correctly multiple-letter motifs in nucleotide sequences", {
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "CA"))[1],
-               3)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "CAN"))[1],
-               3)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "GAAT"))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), "RARARARA"))[1],
-               0)
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "CA")[["start"]],
+               c(8, 1, 6))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "NCA")[["start"]],
+               c(start = 8))
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), "GAAT")[["start"]],
+               c(start = 3))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), "RARARARA")[["start"]],
+               numeric(0))
 })
 
 test_that("find_motifs detects correctly multiple motifs in amino acid sequences", {
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("TY", "TYM")))[1],
-               1)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("PAD", "TY", "GILI")))[1],
-               3)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("GILI", "PAD")))[1],
-               2)
-  expect_equal(dim(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("RARARARA", "GILI")))[1],
-               1)
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("TY", "TYM"))[["start"]],
+               c(start = 4))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("PAD", "TY", "GILI"))[["start"]],
+               c(3, 4, 5))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("GILI", "PAD"))[["start"]],
+               c(5, 3))
+  expect_equal(find_motifs(sq_ami, c("sq1", "sq2", "sq3"), c("RARARARA", "GILI"))[["start"]],
+               c(start = 5))
 })
 
 test_that("find_motifs detects correctly multiple motifs in nucleotide sequences", {
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), c("CA", "CAN")))[1],
-               6)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), c("CAN", "BRRMAA")))[1],
-               3)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), c("GAAT", "CAN")))[1],
-               4)
-  expect_equal(dim(find_motifs(sq_nuc, c("sq1", "sq2", "sq3"), c("RARARARA", "BYBYBYBYBYBY")))[1],
-               0)
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), c("CA", "CAN"))[["start"]],
+               c(8, 1, 6, 8, 1, 6))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), c("CAN", "BRA"))[["start"]],
+               c(4, 2, 3))
+  expect_equal(find_motifs(sq_dna, c("sq1", "sq2", "sq3"), c("GAAT", "CAN"))[["start"]],
+               c(3, 8, 1, 6))
+  expect_equal(find_motifs(sq_rna, c("sq1", "sq2", "sq3"), c("RARARARA", "BYBYBYBYBYBY"))[["start"]],
+               numeric(0))
 })
