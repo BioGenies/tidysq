4a4090ec5bfe95464dee11f39d25182a29ad09a5
Mateusz <matibakala@gmail.com>
2020-07-31 22:27:52 +0200

    first stage of print remake (feat. `cli` package)

--- R/util_format.R
+++ R/util_format.R
@@ -8,18 +8,28 @@ format.sq <- function(x, ...) {
   rep("hidden", length(x))
 }
 
+#' @importFrom cli cli_text style_bold
+#' @export
+obj_print_header.sq <- function(x, ...) {
+  if (length(.get_sq_type(x)) != 1)
+    cli_text("sq (improper subtype!)")
+  else
+    cli_text("{vec_ptype_full(x)} sequences list")
+}
+
 #' @export
 obj_print_data.sq <- function(x, ...) {
   # for some unknown reason had to copy this code from original obj_print_data
   if (length(x) == 0) {
     return()
   }
-  print(format(x), quote = FALSE)
+  print(format(x, ...), quote = FALSE)
   invisible(x)
 }
 
 #' @export
-obj_print_footer.sq <- function(x, ...) {
-  cat("peek-a-boo!")
+obj_print_footer.sq <- function(x, ..., num_lines = 0) {
+  if (length(x) > num_lines)
+    cat("printed ", num_lines, " out of ", length(x), sep = "")
   invisible(x)
 }
