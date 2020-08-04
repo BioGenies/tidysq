4c3263df0194427c21e58fef2aa7e3c01c67b3f2
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-08-04 17:40:29 +0200

    sqapply function

---
+++ inst/include/tidysq/sqapply.h
@@ -0,0 +1,18 @@
+#ifndef TIDYSQ_SQAPPLY_H
+#define TIDYSQ_SQAPPLY_H
+
+#include "types_general.h"
+#include "interface_Operation.h"
+
+namespace tidysq {
+    template <typename TYPE_IN, typename TYPE_OUT, typename ALPHABET>
+    TYPE_OUT sqapply(const TYPE_IN &sq, const OperationSq<typename TYPE_IN::SequenceType, typename TYPE_OUT::SequenceType, ALPHABET> &op) {
+        TYPE_OUT ret(sq.length(), sq.alphabet());
+        for (lensq i = 0; i < sq.length(); i++) {
+            ret[i] = op(sq[i], sq.alphabet());
+        }
+        return ret;
+    }
+}
+
+#endif //TIDYSQ_SQAPPLY_H
