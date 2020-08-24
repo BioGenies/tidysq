99dbc1629926aa13f97d40e3a8139ae35a92c642
Mateusz <matibakala@gmail.com>
2020-08-24 20:26:13 +0200

    shortened is_null_sq() to simple length comparison with 0
    Hopefully there was no hidden point in making it with .apply_sq() and unlist(). Also, current implementation returns logical() instead of NULL, so it's better, avoids creating edge cases.

--- R/is_null_sq.R
+++ R/is_null_sq.R
@@ -44,6 +44,5 @@
 #' @export
 is_null_sq <- function(sq) {
   .validate_sq(sq)
-  unlist(.apply_sq(sq, "char", "none", function(s) length(s) == 0 || 
-                     length(s) == 1 && identical(s, "")))
+  get_sq_lengths(sq) == 0
 }
