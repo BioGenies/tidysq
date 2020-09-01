74ce1413d0cfe50a81ad14f52c3d074ee353ba1b
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-09-01 12:55:38 +0200

    added using Sq::type() in sqapply to reduce type guessing

--- inst/include/tidysq/sqapply.h
+++ inst/include/tidysq/sqapply.h
@@ -7,7 +7,7 @@
 namespace tidysq {
     template <typename TYPE_IN, typename TYPE_OUT>
     TYPE_OUT sqapply(const TYPE_IN &sq, const OperationSq<typename TYPE_IN::SequenceType, typename TYPE_OUT::SequenceType> &op) {
-        TYPE_OUT ret(sq.length(), sq.alphabet());
+        TYPE_OUT ret(sq.length(), sq.alphabet(), sq.type());
         for (lensq i = 0; i < sq.length(); i++) {
             ret[i] = op(sq[i], sq.alphabet());
         }
