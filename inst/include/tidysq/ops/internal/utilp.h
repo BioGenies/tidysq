e0b4d544fef0ad5421d0eb812cabca03da7678cf
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-09-18 15:46:32 +0200

    refined more utility functions for packing

--- inst/include/tidysq/ops/internal/utilp.h
+++ inst/include/tidysq/ops/internal/utilp.h
@@ -3,6 +3,7 @@
 
 #include "tidysq/types/Sequence.h"
 #include "tidysq/types/SequenceProto.h"
+#include "tidysq/types/Alphabet.h"
 
 namespace tidysq::internal {
     template<ProtoType PROTO_OUT>
@@ -11,6 +12,17 @@ namespace tidysq::internal {
     template<>
     inline ElemRaws matchLetter<RAWS>(const letvalue value, const Alphabet &alphabet) {
         return value;
+    }
+
+    template<InternalType INTERNAL, ProtoType PROTO>
+    inline lensq calculatePackedLength(const SequenceProto<INTERNAL, PROTO> &unpacked, const Alphabet &alphabet) {
+        return (alphabet.alphabetSize() * unpacked.size() + 7) / 8;
+    }
+
+    template<InternalType INTERNAL_IN, ProtoType PROTO_IN, InternalType INTERNAL_OUT>
+    inline Sequence<INTERNAL_OUT> reserveSpaceForPacked(const SequenceProto<INTERNAL_IN, PROTO_IN> &unpacked,
+                                                        const Alphabet &alphabet) {
+        return Sequence<INTERNAL_OUT>(calculatePackedLength(unpacked, alphabet), unpacked.size());
     }
 }
 
