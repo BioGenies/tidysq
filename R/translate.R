b605d688a3288b5b5ef529df7490bb2293614d5d
Mateusz <matibakala@gmail.com>
2020-10-04 13:04:19 +0200

    added documentation for translate()

--- R/translate.R
+++ R/translate.R
@@ -1,3 +1,42 @@
+#' Convert DNA or RNA into proteins using genetic code
+#' 
+#' @description This function allows the user to input DNA or RNA sequences and
+#' acquire sequences of corresponding proteins, where correspondence is encoded
+#' in specified table.
+#' 
+#' @param sq an object of class \code{\link{sq}} with either \strong{dna} or
+#' \strong{rna} type
+#' @param table integer number of translation table used, as specified
+#' \href{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi}{here}
+#' 
+#' @return An object of \code{\link[=sq]{class sq}} with \strong{ami} type and
+#' \strong{cln} subtype.
+#' 
+#' @details
+#' DNA and RNA sequences use combinations of three consecutive nucleic acids to
+#' encode one of 22 amino acids. This encoding is called "genetic code".
+#' 
+#' \code{translate()} first splits passed DNA or RNA sequences into
+#' three-letter chunks. Then searches the codon table for the entry where the
+#' key is equal to the current chunk and the value is one letter that encodes
+#' the corresponding protein. These resulting letters are then pasted into one
+#' sequence for each input sequence and objectifized into an \code{sq} object.
+#' 
+#' Due to how the table works, \code{translate()} does not support inputting
+#' uncleaned sequences, as ambiguous letters mostly cannot be translated into
+#' exactly one protein.
+#' 
+#' Moreover, behaviour of this function is undefined whenever cleaned sequence
+#' contain either \code{-} or \code{NA} sign.
+#' 
+#' @seealso \code{\link{clean}}, \code{\link{substitute_letters}} and
+#' \code{\link{typify}} for necessary actions before using \code{translate()}
+#' 
+#' @examples
+#' sq_dna <- construct_sq_dna(c("TACTGGGCATGA", "CAGGTC", "TAGTCCTAG"),
+#'                            is_clean = TRUE)
+#' translate(sq_dna)
+#' 
 #' @export
 translate <- function(sq, table = 1, ...) {
   UseMethod("translate")
