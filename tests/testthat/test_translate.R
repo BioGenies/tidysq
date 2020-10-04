a1735f64d5333e81ff78769ec242b9b983438a89
Mateusz <matibakala@gmail.com>
2020-10-04 12:20:41 +0200

    added most important, value test for translate()

--- tests/testthat/test_translate.R
+++ tests/testthat/test_translate.R
@@ -1,8 +1,11 @@
 # SETUP ----
-sq_dna <- construct_sq_dna(c("TACTGGGCATG", "CAGGTCGGA", "TAGTAGTCCG", "", "ACGGT"),
+sq_dna <- construct_sq_dna(c("TACTGGGCATGA", "CAGGTCGGA", "TAGTAGTCC", "ACGGTG"),
                            is_clean = TRUE)
-sq_rna <- construct_sq_rna(c("UGGCGG", "ACGGUUUCGUU", "UGGAACG", "GGCUCGACAGACUGC"),
+sq_rna <- construct_sq_rna(c("UGGCGG", "ACGGUUUCGUU", "UGGAACG", "GGCUCGACAGACUGC", ""),
                            is_clean = TRUE)
+
+str_dna_translated <- c("YWA*", "QVG", "**S", "TV")
+str_rna_translated <- c("WR", "TVS", "WN", "GSTDC", "")
 
 # AMISQ CLNSQ PROTOTYPE ----
 test_that("translate() returns clean amino acid sq object", {
@@ -12,4 +15,12 @@ test_that("translate() returns clean amino acid sq object", {
   expect_vector(translate(sq_rna),
                 ptype = .construct_sq_ptype("ami", is_clean = TRUE),
                 size = vec_size(sq_rna))
+})
+
+# VALUE COMPUTATION ----
+test_that("translate() returns correct value", {
+  expect_equivalent(as.character(translate(sq_dna)),
+                    str_dna_translated)
+  expect_equivalent(as.character(translate(sq_rna)),
+                    str_rna_translated)
 })
