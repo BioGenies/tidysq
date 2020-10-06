60c94fb29b66f0a093d50237dfc10bfcc91ec906
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-10-06 17:32:56 +0200

    re-enabled C++-level tests

--- src/test-pack.cpp
+++ src/test-pack.cpp
@@ -6,39 +6,31 @@
 using namespace tidysq;
 
 context("test_packing") {
-//  test_that("pack STD RAWS 1") {
-//    ProtoSequence<STD, RAWS> sequence_proto_1{'A', 'C', 'T', 'G'};
-//    ProtoSequence<STD, RAWS> sequence_proto_2{'A', 'A'};
-//    ProtoSequence<STD, RAWS> sequence_proto_3{'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'};
-//
-//    Alphabet alphabet(std::vector<std::string>{"A", "C", "G", "T", "-"}, "!");
-//
-//    ProtoSq<STD, RAWS> sq_proto(
-//        std::vector<ProtoSequence<STD, RAWS>>{sequence_proto_1,
-//                                              sequence_proto_2,
-//                                              sequence_proto_3},
-//        alphabet);
-//
-//    Sq<STD> sq = sq_proto.pack<STD>();
-//
-//  }
-//
-//  test_that("pack RCPP RAWS 1") {
-//    ProtoSequence<RCPP, RAWS> sequence_proto_1{'A', 'C', 'T', 'G'};
-//    ProtoSequence<RCPP, RAWS> sequence_proto_2{'A', 'A'};
-//    ProtoSequence<RCPP, RAWS> sequence_proto_3{'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'};
-//
-//    Rcpp::StringVector alph_content{"A", "C", "G", "T", "-"};
-//    alph_content.attr("na_letter") = "!";
-//
-//    Alphabet alphabet(alph_content);
-//
-//    ProtoSq<RCPP, RAWS> sq_proto = importProtoFromR<RAWS>(
-//        Rcpp::List{sequence_proto_1,
-//                   sequence_proto_2,
-//                   sequence_proto_3},
-//        alph_content);
-//
-//    Sq<RCPP> sq = sq_proto.pack<RCPP>();
-//  }
+  test_that("pack STD RAWS 1") {
+    Alphabet alphabet({"A", "C", "G", "T", "-"}, "!");
+
+    ProtoSq<STD, RAWS> sq_proto({
+        {'A', 'C', 'T', 'G'},
+        {'A', 'A'},
+        {'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'}
+    }, alphabet);
+
+    Sq<STD> sq = sq_proto.pack<STD>();
+  }
+
+  test_that("pack RCPP RAWS 1") {
+    Rcpp::StringVector alph_content{"A", "C", "G", "T", "-"};
+    alph_content.attr("na_letter") = "!";
+
+    Alphabet alphabet(alph_content);
+
+    ProtoSq<RCPP, RAWS> sq_proto = importProtoFromR<RAWS>(
+        Rcpp::List::create(
+                Rcpp::RawVector{'A', 'C', 'T', 'G'},
+                Rcpp::RawVector{'A', 'A'},
+                Rcpp::RawVector{'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'}),
+                alph_content);
+
+    Sq<RCPP> sq = sq_proto.pack<RCPP>();
+  }
 }
