534249da3a41b29d5b5fb5d0dff0a20f43705f40
Mateusz <matibakala@gmail.com>
2020-10-09 14:19:40 +0200

    renamed clean() to remove_ambiguous() and generified it

---
+++ R/remove_ambiguous.R
@@ -0,0 +1,30 @@
+#' @export
+remove_ambiguous <- function(x, by_letter = FALSE, ...) {
+  assert_flag(by_letter)
+  UseMethod("remove_ambiguous")
+}
+
+#' @export
+remove_ambiguous.default <- function(x, by_letter, ...)
+  stop("ambiguous letters are not defined in the context of this class", call. = FALSE)
+
+#' @export
+remove_ambiguous.sq_dna_bsc <- function(x, by_letter, ...) x
+
+#' @export
+remove_ambiguous.sq_dna_ext <- function(x, by_letter, ...)
+  CPP_remove_ambiguous(x, by_letter, get_standard_alphabet("sq_dna_bsc"))
+
+#' @export
+remove_ambiguous.sq_rna_bsc <- function(x, by_letter, ...) x
+
+#' @export
+remove_ambiguous.sq_rna_ext <- function(x, by_letter, ...)
+  CPP_remove_ambiguous(x, by_letter, get_standard_alphabet("sq_rna_bsc"))
+
+#' @export
+remove_ambiguous.sq_ami_bsc <- function(x, by_letter, ...) x
+
+#' @export
+remove_ambiguous.sq_ami_ext <- function(x, by_letter, ...)
+  CPP_remove_ambiguous(x, by_letter, get_standard_alphabet("sq_ami_bsc"))
