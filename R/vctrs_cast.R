73e9170e77e5b82bec8ec82387526c7b21ae7e30
Mateusz <matibakala@gmail.com>
2020-10-09 14:18:54 +0200

    moved encsq_to_list() to as.list.sq_enc()

--- R/vctrs_cast.R
+++ R/vctrs_cast.R
@@ -1,15 +1,8 @@
-# amisq ----
-#' @export
-vec_cast.amisq.amisq <- function(x, to, ...)
-  if (.is_cleaned(x) == .is_cleaned(to)) x else .construct_amisq(as.character(x), .is_cleaned(to))
-#' @export
-vec_cast.amisq.character <- function(x, to, ...) .construct_amisq(x, .is_cleaned(to))
-#' @export
-vec_cast.character.amisq <- function(x, to, ...) unlist(.unpack_from_sq(x, "string"))
-#' @export
-vec_cast.untsq.amisq <- function(x, to, ...) .construct_untsq(as.character(x), alph = alphabet(to))
-#' @export
-vec_cast.amisq.untsq <- function(x, to, ...) typify(x, "ami")
+# sq_enc
+vec_cast.list.sq_enc <- function(x, to, ...)
+  .apply_sq(x, "int", "none", function(s) alphabet(encsq)[s])
+
+# TODO: rewrite all this shit
 
 # dnasq ----
 #' @export
@@ -36,6 +29,19 @@ vec_cast.character.rnasq <- function(x, to, ...) unlist(.unpack_from_sq(x, "stri
 vec_cast.untsq.rnasq <- function(x, to, ...) .construct_untsq(as.character(x), alph = alphabet(to))
 #' @export
 vec_cast.rnasq.untsq <- function(x, to, ...) typify(x, "rna")
+
+# amisq ----
+#' @export
+vec_cast.amisq.amisq <- function(x, to, ...)
+  if (.is_cleaned(x) == .is_cleaned(to)) x else .construct_amisq(as.character(x), .is_cleaned(to))
+#' @export
+vec_cast.amisq.character <- function(x, to, ...) .construct_amisq(x, .is_cleaned(to))
+#' @export
+vec_cast.character.amisq <- function(x, to, ...) unlist(.unpack_from_sq(x, "string"))
+#' @export
+vec_cast.untsq.amisq <- function(x, to, ...) .construct_untsq(as.character(x), alph = alphabet(to))
+#' @export
+vec_cast.amisq.untsq <- function(x, to, ...) typify(x, "ami")
 
 # untsq ----
 #' @export
