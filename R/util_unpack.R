83fe8e4727a0e02178617ed8f80e96db462fe188
Mateusz <matibakala@gmail.com>
2020-10-13 01:09:53 +0200

    filled unpack() with some love and muffins

--- R/util_unpack.R
+++ R/util_unpack.R
@@ -1,4 +1,16 @@
+#' @importFrom checkmate assert_choice
+#' @importFrom checkmate assert_string
 unpack <- function(x, format,
                    NA_letter = getOption("tidysq_NA_letter")) {
-  # TODO: fill with care and love
+  assert_choice(format, c("RAWS", "INTS", "STRINGS", "STRING"))
+  assert_string(NA_letter)
+  
+  op <- switch (
+    format,
+    RAWS = CPP_unpack_RAWS,
+    INTS = CPP_unpack_INTS,
+    STRINGS = CPP_unpack_STRINGS,
+    STRING = CPP_unpack_STRING
+  )
+  op(x)
 }
