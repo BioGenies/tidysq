4e2c877f146f55c1a87cf05ca717b25188e5edc2
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-10-23 18:11:19 +0200

    finished unpacking non-common

--- inst/include/tidysq/ops/OperationUnpack.h
+++ inst/include/tidysq/ops/OperationUnpack.h
@@ -2,7 +2,7 @@
 #define TIDYSQ_OPERATIONUNPACK_H
 
 #include "tidysq/ops/interface/Operation.h"
-#include "tidysq/ops/internal/unpack_simple.h"
+#include "tidysq/ops/internal/unpack_common.h"
 #include "tidysq/ops/internal/unpack_multichar.h"
 #include "tidysq/ops/internal/util.h"
 
@@ -13,13 +13,25 @@ namespace tidysq::ops {
     public:
         ProtoSequence<INTERNAL_OUT, PROTO_OUT> operator()(const Sequence<INTERNAL_IN> &packed,
                                                           const Alphabet &alphabet) const override {
-            ProtoSequence<INTERNAL_OUT, PROTO_OUT> unpacked;
+            ProtoSequence<INTERNAL_OUT, PROTO_OUT> unpacked = internal::reserveSpaceForUnpacked<INTERNAL_IN, INTERNAL_OUT, PROTO_OUT>(packed);
+            internal::unpack_common<INTERNAL_IN, INTERNAL_OUT, PROTO_OUT>(packed, unpacked, alphabet);
+            return unpacked;
+        }
+    };
+
+    template<InternalType INTERNAL_IN, InternalType INTERNAL_OUT>
+    class OperationUnpack<INTERNAL_IN, INTERNAL_OUT, STRING> : public OperationSq<Sequence<INTERNAL_IN>,
+            ProtoSequence<INTERNAL_OUT, STRING>> {
+    public:
+        ProtoSequence<INTERNAL_OUT, STRING> operator()(const Sequence<INTERNAL_IN> &packed,
+                                                          const Alphabet &alphabet) const override {
+            ProtoSequence<INTERNAL_OUT, STRING> unpacked;
             if (alphabet.is_simple()) {
-                unpacked = internal::reserveSpaceForUnpacked<INTERNAL_IN, INTERNAL_OUT, PROTO_OUT>(packed);
-                internal::unpack_simple<INTERNAL_IN, INTERNAL_OUT, PROTO_OUT>(packed, unpacked, alphabet);
+                unpacked = internal::reserveSpaceForUnpacked<INTERNAL_IN, INTERNAL_OUT, STRING>(packed);
+                internal::unpack_common<INTERNAL_IN, INTERNAL_OUT, STRING>(packed, unpacked, alphabet);
             } else {
                 unpacked = {};
-                internal::unpack_multichar_2<INTERNAL_IN, INTERNAL_OUT, PROTO_OUT>(packed, unpacked, alphabet);
+                internal::unpack_multichar_string<INTERNAL_IN, INTERNAL_OUT>(packed, unpacked, alphabet);
             }
             return unpacked;
         }
