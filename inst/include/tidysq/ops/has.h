9b5c0ce5c1fb1e1d77ba46b9b2d1b998a7f6b89b
Mateusz <matibakala@gmail.com>
2020-11-03 12:18:37 +0100

    CPP_has: added handling "$" and ambiguous letters

--- inst/include/tidysq/ops/has.h
+++ inst/include/tidysq/ops/has.h
@@ -2,6 +2,7 @@
 #define TIDYSQ_HAS_H
 
 #include "tidysq/types/Sq.h"
+#include <map>
 
 namespace tidysq {
     namespace internal {
@@ -11,6 +12,42 @@ namespace tidysq {
     std::list<internal::Motif> convert_motifs(const std::vector<std::string>& motifs,
                                               const Alphabet& alph);
 
+    std::unordered_map<ElementStringSimple, std::list<ElementStringSimple>> ambiguousAminoMap = {
+            {'B', {'B', 'D', 'N'}},
+            {'J', {'J', 'I', 'L'}},
+            {'Z', {'Z', 'E', 'Q'}},
+            {'X', {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                          'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}}
+    };
+
+    std::unordered_map<ElementStringSimple, std::list<ElementStringSimple>> ambiguousDNAMap = {
+            {'W', {'W', 'A', 'T'}},
+            {'S', {'S', 'C', 'G'}},
+            {'M', {'M', 'A', 'C'}},
+            {'K', {'K', 'G', 'T'}},
+            {'R', {'R', 'A', 'G'}},
+            {'Y', {'Y', 'C', 'T'}},
+            {'B', {'B', 'S', 'K', 'Y', 'C', 'G', 'T'}},
+            {'D', {'D', 'W', 'K', 'R', 'A', 'G', 'T'}},
+            {'H', {'H', 'W', 'M', 'Y', 'A', 'C', 'T'}},
+            {'V', {'V', 'S', 'M', 'R', 'A', 'C', 'G'}},
+            {'N', {'A', 'C', 'G', 'T', 'W', 'S', 'M', 'K', 'R', 'Y', 'B', 'D', 'H', 'V', 'N'}}
+    };
+
+    std::unordered_map<ElementStringSimple, std::list<ElementStringSimple>> ambiguousRNAMap = {
+            {'W', {'W', 'A', 'U'}},
+            {'S', {'S', 'C', 'G'}},
+            {'M', {'M', 'A', 'C'}},
+            {'K', {'K', 'G', 'U'}},
+            {'R', {'R', 'A', 'G'}},
+            {'Y', {'Y', 'C', 'U'}},
+            {'B', {'B', 'S', 'K', 'Y', 'C', 'G', 'U'}},
+            {'D', {'D', 'W', 'K', 'R', 'A', 'G', 'U'}},
+            {'H', {'H', 'W', 'M', 'Y', 'A', 'C', 'U'}},
+            {'V', {'V', 'S', 'M', 'R', 'A', 'C', 'G'}},
+            {'N', {'A', 'C', 'G', 'U', 'W', 'S', 'M', 'K', 'R', 'Y', 'B', 'D', 'H', 'V', 'N'}}
+    };
+
     namespace internal {
         class Motif {
             const Alphabet &alph_;
@@ -18,6 +55,48 @@ namespace tidysq {
             bool from_start_ = false;
             bool until_end_ = false;
 
+        private:
+            [[nodiscard]] inline std::list<LetterValue> match_value(const ElementStringSimple &letter) {
+                std::list<LetterValue> ret{};
+                std::list<ElementStringSimple> meanings{};
+                // TODO: replace with choosing correct map or skipping due to no suitable map
+                switch (alph_.type()) {
+                    case AMI_BSC:
+                    case AMI_EXT:
+                        // TODO: replace with .contains once C++20 becomes widely acceptable
+                        if (ambiguousAminoMap.count(letter) == 1) {
+                            meanings = ambiguousAminoMap[letter];
+                        } else {
+                            meanings.push_back(letter);
+                        }
+                        break;
+                    case DNA_BSC:
+                    case DNA_EXT:
+                        if (ambiguousDNAMap.count(letter) == 1) {
+                            meanings = ambiguousDNAMap[letter];
+                        } else {
+                            meanings.push_back(letter);
+                        }
+                        break;
+                    case RNA_BSC:
+                    case RNA_EXT:
+                        if (ambiguousRNAMap.count(letter) == 1) {
+                            meanings = ambiguousRNAMap[letter];
+                        } else {
+                            meanings.push_back(letter);
+                        }
+                        break;
+                    default:
+                        meanings.push_back(letter);
+                        break;
+                }
+                for (auto meaning : meanings) {
+                    ret.push_back(alph_.match_value(meaning));
+                }
+                return ret;
+            }
+
+        public:
             Motif(const std::string& motif, const Alphabet& alph) :
                     alph_(alph) {
                 content_ = {};
@@ -36,7 +115,7 @@ namespace tidysq {
                         }
                         else throw std::invalid_argument("'$' cannot appear anywhere other than at the end of motif");
                     }
-                    content_.push_back({alph.match_value(*it)});
+                    content_.push_back(match_value(*it));
                 }
             }
 
@@ -79,13 +158,20 @@ namespace tidysq {
         public:
             template<InternalType INTERNAL>
             [[nodiscard]] bool appears_in(const Sequence<INTERNAL>& sequence) const {
-                SequenceIterator<INTERNAL> it = sequence.begin(alph_);
                 bool contains_motif = empty();
                 if (sequence.originalLength() >= length()) {
                     if (from_start_) {
-                        contains_motif = aligns_with(it, sequence.end(alph_));
+                        if (until_end_) {
+                            contains_motif = (sequence.originalLength() == length()) &&
+                                    aligns_with(sequence.begin(alph_), sequence.end(alph_));
+                        } else {
+                            contains_motif = aligns_with(sequence.begin(alph_), sequence.end(alph_));
+                        }
+                    } else if (until_end_) {
+                        contains_motif = aligns_with(sequence.end(alph_) - length(), sequence.end(alph_));
                     } else {
-                        while (!contains_motif && it < sequence.end(alph_) - length()) {
+                        SequenceIterator<INTERNAL> it = sequence.begin(alph_);
+                        while (!contains_motif && it <= sequence.end(alph_) - length()) {
                             contains_motif = aligns_with(it, sequence.end(alph_));
                             ++it;
                         }
@@ -103,7 +189,7 @@ namespace tidysq {
                                               const Alphabet& alph) {
         std::list<internal::Motif> ret{};
         for (const auto& motif : motifs) {
-            ret.push_back(internal::Motif(motif, alph));
+            ret.emplace_back(motif, alph);
         }
         return ret;
     }
@@ -125,13 +211,7 @@ namespace tidysq {
             });
         }
         // Steps to take:
-        // 1. handle ^ and $ signs
-        // 2. convert motifs to a list of lists of letters
-        //      (if you go for a class, place it within tidysq-internal namespace)
-        //      (each element of inner lists is a set of acceptable letters at this position - stored as bits)
-        //      (store one motif as linked list of arrays)
-        // 3. iterate over sequences and motifs, trying to align them
-        //      (probably for each vector in a list within the list use any_of(letter_vec...))
+        // 1. add support for multicharacter alphabet
         return ret;
     }
 }
