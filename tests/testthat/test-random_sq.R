631188879c57bb0ebf94a9677c833d95436f20f7
Mateusz <matibakala@gmail.com>
2020-11-10 15:55:00 +0100

    fixed args in random_sq tests

--- tests/testthat/test-random_sq.R
+++ tests/testthat/test-random_sq.R
@@ -1,15 +1,15 @@
 # CORRECT PROTOTYPE OF RETURNED VALUE ----
 test_that("random_sq() returns an object of sq class", {
-  expect_vector(random_sq(4, 7, "dna", TRUE),
-                ptype = .construct_sq_ptype("dna", is_clean = TRUE),
+  expect_vector(random_sq(4, 7, "dna_bsc"),
+                ptype = sq_ptype(get_standard_alphabet("dna_bsc"), "dna_bsc"),
                 size = 4)
-  expect_vector(random_sq(1, 26, "ami", FALSE),
-                ptype = .construct_sq_ptype("ami", is_clean = FALSE),
+  expect_vector(random_sq(1, 26, "ami_ext"),
+                ptype = sq_ptype(get_standard_alphabet("ami_ext"), "ami_ext"),
                 size = 1)
 })
 
 test_that("each sequence of random_sq() is of the passed length", {
-  for (sq in random_sq(3, 27, "rna", TRUE)) {
+  for (sq in random_sq(3, 27, "rna_bsc")) {
     expect_equal(attr(sq, "original_length"), 27)
   }
 })
@@ -22,22 +22,22 @@ test_that("using sd argument of random_sq() doesn't generate negative-length seq
   local_mock(rnorm = function(x, mean, sd) {
     seq(from = mean - 2*sd, to = mean + 2*sd, length.out = x)
   })
-  for (sq in random_sq(25, 3, "dna", TRUE, sd = 50)) {
+  for (sq in random_sq(25, 3, "dna_bsc", sd = 50)) {
     expect_gte(attr(sq, "original_length"), 0)
   }
 })
 
 # EDGE CASES ----
 test_that("random_sq() can generate 0 sequences", {
-  expect_vector(random_sq(0, 13, "rna", FALSE),
-                ptype = .construct_sq_ptype("rna", is_clean = FALSE),
+  expect_vector(random_sq(0, 13, "rna_ext"),
+                ptype = sq_ptype(get_standard_alphabet("rna_ext"), "rna_ext"),
                 size = 0)
 })
 
 test_that("random_sq() can generate 0-length sequences", {
-  empty_sq <- random_sq(3, 0, "ami", TRUE)
+  empty_sq <- random_sq(3, 0, "ami_bsc")
   expect_vector(empty_sq,
-                ptype = .construct_sq_ptype("ami", is_clean = TRUE),
+                ptype = sq_ptype(get_standard_alphabet("ami_bsc"), "ami_bsc"),
                 size = 3)
   for (sq in empty_sq) {
     expect_length(sq, 0)
