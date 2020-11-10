35132fad4f29d53a597f16695fda836d0a024428
Mateusz <matibakala@gmail.com>
2020-11-10 15:08:39 +0100

    added skipped tests for remove_ambiguous (old clean)

---
+++ tests/testthat/test-remove_ambiguous.R
@@ -0,0 +1,72 @@
+# SETUP ----
+sq_ami <- sq(c("MIAANYTWIL","TIAALGNIIYRAIE", "NYERTGHLI", "MAYXXXIALN"),
+             alphabet = "ami_ext")
+sq_ami_2 <- sq(c("MIAANYTWIL","TIAALGNIIYRAIE", "NYERTGHLI", ""),
+               alphabet = "ami_bsc")
+sq_ami_3 <- sq(c("MIAANYTWIL","TIAALGNIIYRAIE", "NYERTGHLI", "MAYIALN"),
+               alphabet = "ami_bsc")
+sq_dna <- sq(c("ATGCAGGA", "GACCGAACGAN", "TGACGAGCTTA", "ACTNNAGCN"),
+             alphabet = "dna_ext")
+sq_dna_2 <- sq(c("ATGCAGGA", "", "TGACGAGCTTA", ""),
+               alphabet = "dna_bsc")
+sq_dna_3 <- sq(c("ATGCAGGA", "GACCGAACGA", "TGACGAGCTTA", "ACTAGC"),
+               alphabet = "dna_bsc")
+sq_rna <- sq(c("UGGCGNNGBV", "ACGGUUUCGUU", "UBBGDGAACG", "GGCUCGACAGACUGC"),
+             alphabet = "rna_ext")
+sq_rna_2 <- sq(c("", "ACGGUUUCGUU", "", "GGCUCGACAGACUGC"),
+               alphabet = "rna_bsc")
+sq_rna_3 <- sq(c("UGGCGG", "ACGGUUUCGUU", "UGGAACG", "GGCUCGACAGACUGC"),
+               alphabet = "rna_bsc")
+
+# CORRECT PROTOTYPE OF RETURNED VALUE ----
+test_that("remove_ambiguous() returns an sq object with _bsc class", {
+  skip("CPP_find_invalid_letters() not implemented")
+  expect_vector(remove_ambiguous(sq_ami),
+                ptype = sq_ptype(get_standard_alphabet("ami_bsc"), "ami_bsc"),
+                size = vec_size(sq_ami))
+  expect_vector(remove_ambiguous(sq_dna_3),
+                ptype = sq_ptype(get_standard_alphabet("dna_bsc"), "dna_bsc"),
+                size = vec_size(sq_dna_3))
+  expect_vector(remove_ambiguous(sq_rna_2),
+                ptype = sq_ptype(get_standard_alphabet("rna_bsc"), "rna_bsc"),
+                size = vec_size(sq_rna_2))
+})
+
+# VALUE COMPUTATION ----
+test_that("remove_ambiguous() removes whole sequences correctly", {
+  skip("CPP_find_invalid_letters() not implemented")
+  expect_equal(remove_ambiguous(sq_ami),
+               sq_ami_2)
+  expect_equal(remove_ambiguous(sq_dna),
+               sq_dna_2)
+  expect_equal(remove_ambiguous(sq_rna),
+               sq_rna_2)
+})
+
+test_that("remove_ambiguous() removes whole sequences when `by_letter = FALSE`", {
+  skip("CPP_find_invalid_letters() not implemented")
+  expect_equal(remove_ambiguous(sq_ami, by_letter = FALSE),
+               sq_ami_2)
+  expect_equal(remove_ambiguous(sq_dna, by_letter = FALSE),
+               sq_dna_2)
+  expect_equal(remove_ambiguous(sq_rna, by_letter = FALSE),
+               sq_rna_2)
+})
+
+test_that("remove_ambiguous() removes letters only when `by_letter = TRUE`", {
+  skip("CPP_find_invalid_letters() not implemented")
+  expect_equal(remove_ambiguous(sq_ami, by_letter = TRUE),
+               sq_ami_3)
+  expect_equal(remove_ambiguous(sq_dna, by_letter = TRUE),
+               sq_dna_3)
+  expect_equal(remove_ambiguous(sq_rna, by_letter = TRUE),
+               sq_rna_3)
+})
+
+# NO CHANGES IF ALREADY TARGET CLASS ----
+test_that("remove_ambiguous() doesn't affect basic alphabet sequences", {
+  skip("CPP_find_invalid_letters() not implemented")
+  expect_identical(remove_ambiguous(sq_ami_2), sq_ami_2)
+  expect_identical(remove_ambiguous(sq_dna_3), sq_dna_3)
+  expect_identical(remove_ambiguous(sq_rna_2), sq_rna_2)
+})
