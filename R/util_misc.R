7605a19d4e729a85f5053051fea0e50bce14a8b6
Mateusz <matibakala@gmail.com>
2020-11-21 19:25:17 +0100

    removed unused function .set_original_length

--- R/util_misc.R
+++ R/util_misc.R
@@ -1,11 +1,3 @@
-.set_original_length <- function(x, orig_lengths) {
-  if (length(x) == 0) return(x)
-  for (index in 1:length(x)) {
-    attr(x[[index]], "original_length") <- orig_lengths[index]
-  }
-  x
-}
-
 # TODO: outsource
 .get_readable_file <- function(file) {
   if (test_file_exists(file)) {
