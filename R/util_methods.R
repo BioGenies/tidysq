bc3a9e09c9eb69ab97a0bae4123113be21dac59a
Mateusz <matibakala@gmail.com>
2020-11-23 11:17:57 +0100

    made as.sq.character() an alias to sq() instead of calling sq() in body

--- R/util_methods.R
+++ R/util_methods.R
@@ -22,11 +22,7 @@ as.sq.default <- function(x, ...)
   stop("'as.sq' cannot handle objects with this class")
 
 #' @export
-as.sq.character <- function(x,
-                            alphabet = NULL,
-                            NA_letter = getOption("tidysq_NA_letter"),
-                            safe_mode = getOption("tidysq_safe_mode"))
-  sq(x, alphabet, NA_letter, safe_mode)
+as.sq.character <- sq
 
 #' Convert sq object into character vector
 #' 
