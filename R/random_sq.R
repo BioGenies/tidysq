807ff8645306114580c52f74e6214cb5185e6166
Mateusz <matibakala@gmail.com>
2020-12-01 16:29:07 +0100

    added examples for random_sq

--- R/random_sq.R
+++ R/random_sq.R
@@ -24,6 +24,22 @@
 #' Letter '*' is not used in generating \strong{ami} sequences. If parameter
 #' \code{sd} is passed, then all generated negative values are replaced with 0s.
 #'
+#' @examples
+#' # Setting seed for reproducibility
+#' set.seed(16)
+#'
+#' # Generating random sequences
+#' random_sq(10, 10, "ami_bsc")
+#' random_sq(25, 18, "rna_bsc", sd = 6)
+#' random_sq(50, 8, "dna_ext", sd = 3)
+#' random_sq(6, 100, "ami_bsc", use_gap = TRUE)
+#'
+#' # Passing whole alphabet instead of type
+#' random_sq(4, 12, c("Pro", "Gly", "Ala", "Met", "Cys"))
+#'
+#' # Generating empty sequences (why would anyone though)
+#' random_sq(8, 0, "rna_ext")
+#'
 #' @family io_functions
 #' @export
 random_sq <- function(n, len, alphabet, sd = NULL, use_gap = FALSE) {
