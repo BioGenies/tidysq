3c358e10f8927b1685a4696070db0e61d462295f
Mateusz <matibakala@gmail.com>
2020-12-03 10:44:58 +0100

    added family, docs and examples to reverse

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -2,6 +2,7 @@ list(
   rd_family_title = list(
     io_functions = "Functions from I/O module:",
     cleaning_functions = "Functions that clean sequences:",
-    bio_functions = "Functions interpreting sq in biological context:"
+    bio_functions = "Functions interpreting sq in biological context:",
+    order_functions = "Functions that affect order of elements:"
   )
 )
