1c5a26ea288e7ae218f6d601948d617b41528a99
Mateusz <matibakala@gmail.com>
2020-12-04 17:30:35 +0100

    updated docs & examples for write_fasta

--- R/write_fasta.R
+++ R/write_fasta.R
@@ -2,7 +2,8 @@
 #'
 #' @templateVar name_null_ok FALSE
 #' 
-#' Writes \code{\link[=sq-class]{sq}} objects with their names to a fasta file.
+#' @description Writes \code{\link[=sq-class]{sq}} objects with their names to
+#' a fasta file.
 #'
 #' @template x
 #' @template name
@@ -12,15 +13,29 @@
 #'  Maximum number of characters to put in each line of file. Must be positive.
 #' @template NA_letter
 #'
+#' @details
+#' Whenever a name has more letters than \code{width} parameter, nothing
+#' happens, as only sequences are splitted to fit within designated space.
+#'
+#' @examples
+#' \dontrun{
+#' sq_dna <- sq(c("ACTGCTG", "CTTAGA", "CCCT", "CTGAATGT"),
+#'              alphabet = "dna_bsc")
+#' write_fasta(sq_dna,
+#'             c("bat", "cat", "rat", "elephant_swallowed_by_A_snake"),
+#'             "~/fasta_rubbish/example.fasta")
+#' }
+#'
+#' @family io_functions
 #' @export
-write_fasta <- function(x, names, file, 
+write_fasta <- function(x, name, file,
                         width = 80,
                         NA_letter = getOption("tidysq_NA_letter")) {
   assert_class(x, "sq")
-  assert_character(names, len = vec_size(x), any.missing = FALSE)
+  assert_character(name, len = vec_size(x), any.missing = FALSE)
   assert_string(file)
   assert_count(width, positive = TRUE)
   assert_string(NA_letter, min.chars = 1)
   
-  CPP_write_fasta(x, names, file, width, NA_letter)
+  CPP_write_fasta(x, name, file, width, NA_letter)
 }
