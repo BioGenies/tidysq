ca8d5fcc7cf60f15c637844c12958bfa6d7e7245
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-12-08 19:11:43 +0100

    added overloads of sqapply for l-values references to Operation

--- inst/include/tidysq/sqapply.h
+++ inst/include/tidysq/sqapply.h
@@ -7,7 +7,7 @@ namespace tidysq {
     template<typename VECTOR_IN, typename ELEMENT_IN,
              typename VECTOR_OUT, typename ELEMENT_OUT>
     VECTOR_OUT sqapply(const VECTOR_IN &vector_in,
-                       ops::OperationVectorToVector<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT> &&operation) {
+                       ops::OperationVectorToVector<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT> &operation) {
         VECTOR_OUT ret = operation.initialize_vector_out(vector_in);
         for (LenSq i = 0; i < vector_in.size(); i++) {
             ret[i] = operation(vector_in[i]);
@@ -18,7 +18,14 @@ namespace tidysq {
     template<typename VECTOR_IN, typename ELEMENT_IN,
             typename VECTOR_OUT, typename ELEMENT_OUT>
     VECTOR_OUT sqapply(const VECTOR_IN &vector_in,
-                       ops::OperationVectorToVector<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT> &&operation,
+                       ops::OperationVectorToVector<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT> &&operation) {
+        return sqapply<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT>(vector_in, operation);
+    }
+
+    template<typename VECTOR_IN, typename ELEMENT_IN,
+            typename VECTOR_OUT, typename ELEMENT_OUT>
+    VECTOR_OUT sqapply(const VECTOR_IN &vector_in,
+                       ops::OperationVectorToVector<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT> &operation,
                        const LenSq from,
                        const LenSq to) {
         VECTOR_OUT ret = operation.initialize_vector_out(vector_in, from, to);
@@ -26,5 +33,14 @@ namespace tidysq {
             ret[i] = operation(vector_in[i + from]);
         }
         return ret;
+    }
+
+    template<typename VECTOR_IN, typename ELEMENT_IN,
+            typename VECTOR_OUT, typename ELEMENT_OUT>
+    VECTOR_OUT sqapply(const VECTOR_IN &vector_in,
+                       ops::OperationVectorToVector<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT> &&operation,
+                       const LenSq from,
+                       const LenSq to) {
+        return sqapply<VECTOR_IN, ELEMENT_IN, VECTOR_OUT, ELEMENT_OUT>(vector_in, operation, from, to);
     }
 }
