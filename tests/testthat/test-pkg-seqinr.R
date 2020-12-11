43a7fa88eb7364bb7692558fdbb22617937df8f4
Mateusz <matibakala@gmail.com>
2020-12-11 14:07:13 +0100

    changed names_1/2 to names_dna/ami (more descriptive)

--- tests/testthat/test-pkg-seqinr.R
+++ tests/testthat/test-pkg-seqinr.R
@@ -11,16 +11,16 @@ seqinr_ami <- lapply(str_ami, function(x)
   seqinr::as.SeqFastaAA(seqinr::s2c(x)))
 
 # Names below are just chosen for extravagance, not political reasons
-names_1 <- c("balalaika", "perestroyka", "sojuz", "oncePutin", "LenOut")
-names_2 <- c("Unionists", "Confederates", "Mexicans")
+names_dna <- c("balalaika", "perestroyka", "sojuz", "oncePutin", "LenOut")
+names_ami <- c("Unionists", "Confederates", "Mexicans")
 
 seqinr_dna_n <- mapply(
   function(x, name) seqinr::as.SeqFastadna(seqinr::s2c(x), name = name),
-  str_dna, names_1, SIMPLIFY = FALSE, USE.NAMES = FALSE
+  str_dna, names_dna, SIMPLIFY = FALSE, USE.NAMES = FALSE
 )
 seqinr_ami_n <- mapply(
   function(x, name) seqinr::as.SeqFastaAA(seqinr::s2c(x), name = name),
-  str_ami, names_2, SIMPLIFY = FALSE, USE.NAMES = FALSE
+  str_ami, names_ami, SIMPLIFY = FALSE, USE.NAMES = FALSE
 )
 
 # IMPORT ----
@@ -30,7 +30,7 @@ test_that("correctly imports seqinr::SeqFastadna", {
   expect_identical(import_sq(seqinr_dna_n, separate = FALSE)[["sq"]],
                    sq_dna)
   expect_identical(import_sq(seqinr_dna_n, separate = FALSE)[["name"]],
-                   names_1)
+                   names_dna)
 })
 
 test_that("correctly imports seqinr::SeqFastaAA", {
@@ -39,20 +39,20 @@ test_that("correctly imports seqinr::SeqFastaAA", {
   expect_identical(import_sq(seqinr_ami_n, separate = FALSE)[["sq"]],
                    sq_ami)
   expect_identical(import_sq(seqinr_ami_n, separate = FALSE)[["name"]],
-                   names_2)
+                   names_ami)
 })
 
 # EXPORT ----
 test_that("correctly exports sq object to seqinr::SeqFastadna", {
   expect_identical(export_sq(sq_dna, "seqinr::SeqFastadna"),
                    seqinr_dna)
-  expect_identical(export_sq(sq_dna, "seqinr::SeqFastadna", name = names_1),
+  expect_identical(export_sq(sq_dna, "seqinr::SeqFastadna", name = names_dna),
                    seqinr_dna_n)
 })
 
 test_that("correctly exports sq object to seqinr::SeqFastaAA", {
   expect_identical(export_sq(sq_ami, "seqinr::SeqFastaAA"),
                    seqinr_ami)
-  expect_identical(export_sq(sq_ami, "seqinr::SeqFastaAA", name = names_2),
+  expect_identical(export_sq(sq_ami, "seqinr::SeqFastaAA", name = names_ami),
                    seqinr_ami_n)
 })
