f0e26d53e40071dbca4855e27fcc1813874e77b6
Laura <erdaradun.gaztea@gmail.com>
2022-03-18 23:06:04 +0100

    change argument name to avoid name clash with sqapply

--- tests/testthat/setup-tests.R
+++ tests/testthat/setup-tests.R
@@ -1,15 +1,15 @@
 # ERROR FOR NON-SQ OBJECTS ----
-test_sq_only <- function(fun, ..., .data.frame_ok = FALSE) {
+test_sq_only <- function(.func, ..., .data.frame_ok = FALSE) {
   test_message <- if (.data.frame_ok)
     "the first argument must be of sq or data.frame class" else
       "the first argument must be of sq class"
   
   test_that(test_message, {
-    expect_s3_class(rlang::catch_cnd(fun(1:7, ...)),
+    expect_s3_class(rlang::catch_cnd(.func(1:7, ...)),
                     "error_no_method")
-    expect_s3_class(rlang::catch_cnd(fun(LETTERS, ...)),
+    expect_s3_class(rlang::catch_cnd(.func(LETTERS, ...)),
                     "error_no_method")
-    expect_s3_class(rlang::catch_cnd(fun(list(mean, sum, sd), ...)),
+    expect_s3_class(rlang::catch_cnd(.func(list(mean, sum, sd), ...)),
                     "error_no_method")
   })
 }
