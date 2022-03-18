1eae7304f88a806b2ec149352a47c62a4510cf28
Laura <erdaradun.gaztea@gmail.com>
2022-03-18 23:44:46 +0100

    allow custom message for "no method" error

--- R/util-conditions.R
+++ R/util-conditions.R
@@ -1,9 +1,15 @@
-stop_no_method <- function(func, x) {
+stop_no_method <- function(func, x, msg = NULL, msg_append = NULL) {
+  if (is.null(msg))
+    msg <- paste0("no suitable ", substitute(func),
+                  "() method for object of classes <",
+                  paste0(class(x), collapse = ", "), ">")
+  
+  if (!is.null(msg_append))
+    msg <- paste0(msg, "; ", msg_append)
+  
   err <- structure(
     list(
-      message = paste0("no suitable ", substitute(func),
-                       "() method for object of classes <",
-                       paste0(class(x), collapse = ", "), ">"),
+      message = msg,
       call = NULL,
       func = substitute(func),
       classes = class(x)
