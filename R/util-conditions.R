4155598da04b599c9fd8c12b26ed98cf80bf37eb
Laura <erdaradun.gaztea@gmail.com>
2022-03-18 23:48:14 +0100

    change error message to be generated from a function

--- R/util-conditions.R
+++ R/util-conditions.R
@@ -1,9 +1,9 @@
 stop_no_method <- function(func, x, msg = NULL, msg_append = NULL) {
   if (is.null(msg))
-    msg <- paste0("no suitable ", substitute(func),
-                  "() method for object of classes <",
-                  paste0(class(x), collapse = ", "), ">")
-  
+    msg <- function(cls) paste0("no suitable ", substitute(func),
+                                "() method for object of classes <",
+                                paste0(cls, collapse = ", "), ">")
+  msg <- msg(class(x))
   if (!is.null(msg_append))
     msg <- paste0(msg, "; ", msg_append)
   
