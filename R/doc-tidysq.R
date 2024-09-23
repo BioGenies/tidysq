4da556ff9119394bb580d15bf2f6697c388e62c9
DominikRafacz <dominikrafacz@gmail.com>
2024-09-23 21:14:40 +0200

    adjust documentation to the newest roxygen guidelines

--- R/doc-tidysq.R
+++ R/doc-tidysq.R
@@ -1,16 +1,15 @@
 #' tidysq: tidy analysis of biological sequences
 #'
-#' @description The \code{tidysq} package is a toolbox for the analysis of 
+#' @description The \code{tidysq} package is a toolbox for the analysis of
 #' biological sequences in a tidy way.
 #' @author Michal Burdukiewicz, Dominik Rafacz, Laura Bąkała, Leon Eyrich Jessen
-#' @docType package
 #' @importFrom stats rnorm setNames
 #' @importFrom tibble as_tibble tibble
 #' @import checkmate
 #' @import vctrs
 #' @name tidysq-package
 #' @aliases tidysq
-NULL
+"_PACKAGE"
 
 #' @useDynLib tidysq, .registration=TRUE
 #' @importFrom Rcpp sourceCpp
